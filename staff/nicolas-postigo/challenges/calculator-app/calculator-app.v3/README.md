e1cb1c7dab86e968006e4bc13806bb6af64330b5
Nicolas <nicolaspostigoausere@hotmail.es>
2020-10-07 11:08:14 +0200

    add challenges

---
+++ staff/nicolas-postigo/challenges/calculator-app/calculator-app.v3/README.md
@@ -0,0 +1,24 @@
+# Calculator App    
+
+
+specs. for calculator app
+
+
+![](./prueba.PNG)
+
+## How it works
+
+it displays result for differents operations.
+
+### The structure
+- HTML
+the app structure in terms of HTML is based on the head, and the body, which is split in:
+<br>*16 buttons 
+    <br> ->numbers from 0 to 9
+    <br> ->operations symbol
+    <br> ->equal symbol
+<br>*1 display where result is displayed
+- CSS
+- JAVASCRIPT
+
+
---
+++ staff/nicolas-postigo/challenges/calculator-app/calculator-app/main.js
@@ -0,0 +1,132 @@
+var buttons = document.querySelectorAll('button')
+var display = document.querySelector('.mostrador')
+
+var current = ''
+var aux
+var operation
+var deleteNumbers = '' 
+var event 
+var number
+
+function.onclick = addNumber(number) {
+    current = current + number 
+    display.innerText = current
+}
+
+buttons[1].onclick = addNumber(7) 
+
+buttons[2].onclick = addNumber(8) 
+
+
+buttons[3].onclick = function() {
+    current = current + '9'
+    display.innerText = current
+}
+
+buttons[4].onclick = function() {
+    aux = current
+    operation = '/'
+    current = ''
+}
+
+buttons[5].onclick = function() {
+    current = current + '4'
+    display.innerText = current
+}
+
+buttons[6].onclick = function() {
+    current = current + '5'
+    display.innerText = current
+}
+
+buttons[7].onclick = function() {
+    current = current + '6'
+    display.innerText = current
+}
+
+buttons[8].onclick = function() {
+    aux = current
+    operation = 'x'
+    current = ''
+}
+
+buttons[9].onclick = function() {
+    current = current + '1'
+    display.innerText = current
+}
+
+buttons[10].onclick = function() {
+    current = current + '2'
+    display.innerText = current
+}
+
+buttons[11].onclick = function() {
+    current = current + '3'
+    display.innerText = current
+}
+
+buttons[12].onclick = function() {
+    aux = current
+    operation = '-'
+    current = ''
+}
+
+buttons[13].onclick = function() {
+    current = deleteNumbers
+    display.innerText = deleteNumbers
+}
+
+buttons[14].onclick = function() {
+    current = current + '0'
+    display.innerText = current
+}
+
+buttons[15].onclick = function() {
+    current = current + '.'
+    display.innerText = current
+}
+
+buttons[16].onclick = function() {
+    aux = current
+    operation = '+'
+    current = ''
+}
+
+buttons[17].onclick = function() {
+    var result = calculate(Number(aux), operation, Number(current))
+    current = result
+    display.innerText = current
+}
+
+
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function calculate(a, operation, b) {
+    if (operation === '+') {
+        return add(a, b)
+    }
+    else if (operation === '-') {
+        return subtract(a,b)
+    }
+    else if (operation === '/') {
+        return divide(a,b)
+    }
+    else if (operation === 'x') {
+        return multiply(a,b)
+    }
+}
