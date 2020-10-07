726c3e9edd620a124fea2c21838b6b3f41d219b2
mariajbellido <54066498+mariajbellido@users.noreply.github.com>
2020-10-07 12:21:09 +0200

    missing ; added

--- staff/mariajesus-bellido/challenges/calculator-app/main.js
+++ staff/mariajesus-bellido/challenges/calculator-app/main.js
@@ -4,8 +4,8 @@
 //Transformación del dato una vez recogido (String vacía '') -> valor conseguimos que desaparezca el valor
 
 
-var current = ''
-var aux = ''
+var current = '';
+var aux = '';
 var resetValue = '';
 
 // 1 - Selección de todos los botones 
@@ -17,83 +17,83 @@ var display = document.querySelector('span');
 //"aux" recogerá el valor para añadirlo a "current"
 // ahora ya vaciaremos / limpiamos el display con '' (empty string)
 
-aux = current
-current = ''
+aux = current;
+current = '';
 
 // Botones del 0 al 9  (orden ascendente) 
 buttons [13].onclick = function () { 
-    current = current + '0'
-    display.innerText = current
+    current = current + '0';
+    display.innerText = current;
 }
 
 buttons [8].onclick = function () { 
-    current = current + '1'
-    display.innerText = current
+    current = current + '1';
+    display.innerText = current;
 }
 
 buttons [9].onclick = function () { 
-    current = current + '2'
-    display.innerText = current
+    current = current + '2';
+    display.innerText = current;
 }
 
 buttons [10].onclick = function () { 
-    current = current + '3'
-    display.innerText = current
+    current = current + '3';
+    display.innerText = current;
 }
 
 buttons [4].onclick = function () { 
-    current = current + '4'
-    display.innerText = current
+    current = current + '4';
+    display.innerText = current;
 }
 
 buttons [5].onclick = function () { 
-    current = current + '5'
-    display.innerText = current
+    current = current + '5';
+    display.innerText = current;
 }
 
 buttons [6].onclick = function () { 
-    current = current + '6'
-    display.innerText = current
+    current = current + '6';
+    display.innerText = current;
 }
 
 buttons [0].onclick = function () { 
-    current = current + '7'
-    display.innerText = current 
+    current = current + '7';
+    display.innerText = current;
 }
 
 buttons [1].onclick = function () { 
-    current = current + '8' 
-    display.innerText = current 
+    current = current + '8';
+    display.innerText = current; 
 }
 
 buttons [2].onclick = function () { 
-    current = current + '9'
-    display.innerText = current
+    current = current + '9';
+    display.innerText = current;
 }
 
 // Botones - Operaciones matemáticas (división, multiplicación, resta y suma) 
 
 
 buttons [3].onclick = function () { 
-    aux = current
+    aux = current;
     operator = '/';
     current = '';
 }
 
 buttons [7].onclick = function () { 
-    aux = current
+    aux = current;
     operator = '*';
     current = '';
 }
 
 buttons [11].onclick = function () { 
-    aux = current
+    aux = current;
     operator = '-';
     current = '';
 }
 
 buttons [15].onclick = function () { 
-    aux = current
+    aux = current;
     operator = '+';
     current = '';
 }
