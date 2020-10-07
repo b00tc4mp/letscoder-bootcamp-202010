29ca79a5afc8828c4a28ac49119be406b81a1d8d
Marta <marta.cg10@gmail.com>
2020-10-07 11:08:13 +0200

    add challenges

---
+++ staff/marta-cervera/challenges/Tic-tac-toe/main.js
@@ -0,0 +1,21 @@
+//declaramos las variables: simbolo( que tiene que ir cambiando según el turno del jugador), estado, y la posicion ganadora
+
+var symbol = "X"
+var state = [];
+
+var buttons = document.querySelectorAll ("button");
+
+var button 0 = button [0]
+buttons [0].onclick = function () {
+    state = symbol;
+
+var button 1 = buttons [1]
+    buttons [1].onclick = function () {
+        state = symbol
+    }
+buttons [2].onclick = function () {
+        state = symbol
+    }
+buttons [0].onclick = function () {
+            state = symbol
+}
