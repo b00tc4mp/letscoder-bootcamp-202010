d3becb4bb635a52a63ae5074ed79e76d5c9c58be
Toño <salvatuidea@gmail.com>
2020-10-07 19:51:01 +0200

    first version

--- staff/antonio-izquierdo/semantic-html/main.js
+++ staff/antonio-izquierdo/semantic-html/main.js
@@ -6,8 +6,9 @@ function printSemanticElements(elements) {
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i]
 
-        // TODO do not print tags DIV, SPAN, SCRIPT, NOSCRIPT, STYLE, IFRAME
-        console.log(element.tagName)
+if ((element.tagName !== "DIV")&&(element.tagName !== "SPAN")&&(element.tagName !== "SCRIPT")&&(element.tagName !== "NOSCRIPT")&&(element.tagName !== "STYLE")&&(element.tagName !== "IFRAME"))
+
+            console.log(element.tagName)
 
         printSemanticElements(element.children)
     }
