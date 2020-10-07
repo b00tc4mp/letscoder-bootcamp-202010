3597fab49249b2b6e4451fa4b863808f9b1d9697
MarioRubio81 <m.rubiogonzalez@gmail.com>
2020-10-07 11:08:12 +0200

    add challenges

---
+++ staff/mario-rubio/challenges/tic-tac-toe-app/main.js
@@ -0,0 +1,13 @@
+
+
+function TurnO() {
+    var PlayerO = document.write("Tu turno, Jugador 1!!!") 
+    return PlayerO
+}
+TurnO()
+
+function TurnX() {
+    var PlayerX = document.write("Tu turno, Jugador 2!!!") 
+    return PlayerX
+}
+TurnX()
