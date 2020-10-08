31294738b94027cc01f0cfdfb1c8bdcb57f46255
manuelbarzi <manuelbarzi@gmail.com>
2020-10-08 12:00:02 +0200

    add solution to the script of semantic html

--- staff/manuel-barzi/semantic-html/main.js
+++ staff/manuel-barzi/semantic-html/main.js
@@ -2,14 +2,41 @@ var children = document.body.children
 
 printSemanticElements(children)
 
-function printSemanticElements(elements) {
+function printSemanticElements(elements, indent) {
+    if (indent === undefined) indent = 1
+
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i]
 
         // TODO do not print tags DIV, SPAN, SCRIPT, NOSCRIPT, STYLE, IFRAME
 
-        console.log(element.tagName)
+        /*
+        if (element.tagName === 'DIV' || element.tagName === 'SPAN') {
+            // NOOP
+        } else console.log(element.tagName)
+        */
 
-        printSemanticElements(element.children)
+        /*
+        if (!(element.tagName === 'DIV' || element.tagName === 'SPAN'))
+            console.log(element.tagName)
+        */
+
+        /*
+        if ((element.tagName !== 'DIV') && (element.tagName !== 'SPAN')) 
+            console.log(element.tagName)
+        */
+
+        /*
+        var indentation = ''
+        
+        for (var j = 0; j < indent; j++) indentation = indentation + '\t'
+        */
+
+        var indentation = '\t'.repeat(indent)
+
+        if (element.tagName !== 'DIV' && element.tagName !== 'SPAN')
+            console.log(indentation + element.tagName)
+
+        printSemanticElements(element.children, indent + 1)
     }
 }
