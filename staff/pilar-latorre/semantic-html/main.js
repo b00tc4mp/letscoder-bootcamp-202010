b0e1f8edf644aac4d6879e8cd284e1370957b9ec
Pilar <pilarlatorre82@gmail.com>
2020-10-08 16:48:42 +0200

    update semantic

--- staff/pilar-latorre/semantic-html/main.js
+++ staff/pilar-latorre/semantic-html/main.js
@@ -2,15 +2,21 @@ var children = document.body.children
 
 printSemanticElements(children)
 
-function printSemanticElements(elements) {
+function printSemanticElements(elements, indent) {
+    if (indent == undefined) indent = 1
+
     for (var i = 0; i < elements.length; i++) {
         var element = elements[i]
 
-if ((element.tagName !== "DIV")&&(element.tagName !== "SPAN")&&(element.tagName !== "SCRIPT")&&(element.tagName !== "NOSCRIPT")&&(element.tagName !== "STYLE")&&(element.tagName !== "IFRAME"))
+    var indentation= ''
 
-            console.log(element.tagName)
+    for (var j = 0; j <= indent; j++) indentation = indentation + '\t'
 
-        printSemanticElements(element.children)
+    if ((element.tagName !== "DIV")&&(element.tagName !== "SPAN")&&(element.tagName !== "SCRIPT")&&(element.tagName !== "NOSCRIPT")&&(element.tagName !== "STYLE")&&(element.tagName !== "IFRAME"))
+
+            console.log(indentation + element.tagName)
+
+        printSemanticElements(element.children, indent + 1)
     }
 }
 
