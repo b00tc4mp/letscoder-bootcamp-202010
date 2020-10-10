e03e6be3ef3b834c967772a118af88c566384732
MarioRubio81 <m.rubiogonzalez@gmail.com>
2020-10-10 11:43:20 +0200

    dssd

--- staff/mario-rubio/semantic-html/semantic-html.js
+++ staff/mario-rubio/semantic-html/semantic-html.js
@@ -15,3 +15,29 @@ if ((element.tagName != "DIV" ) && (element.tagName != "SPAN" ) && (element.tagN
         printSemanticElements(element.children)
     }
 }
+
+
+       var indentation = '\t'.repeat(indent)
+
+       if (element.tagName !== 'DIV' && element.tagName !== 'SPAN')
+           console.log(indentation + element.tagName)
+
+       printSemanticElements(element.children, indent + 1)
+   }
+} 
+
+function printSemanticElements(elements, indent) {
+   if (elements === undefined) elements = document.body.children
+   if (indent === undefined) indent = 1
+
+   for (var i = 0; i < elements.length; i++) {
+       var element = elements[i]
+
+       var indentation = '\t'.repeat(indent)
+
+       if (element.tagName !== 'DIV' && element.tagName !== 'SPAN')
+           console.log(indentation + element.tagName)
+
+       printSemanticElements(element.children, indent + 1)
+   }
+} 
