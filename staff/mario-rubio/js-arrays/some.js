a00e51663941dcae1169c9c3b390f11b447755ca
MarioRubio81 <m.rubiogonzalez@gmail.com>
2020-10-13 12:51:32 +0200

    more adds

---
+++ staff/mario-rubio/js-arrays/some.js
@@ -0,0 +1,17 @@
+console.log ("DEMO some")
+
+nums = [1, 4, 5, 7, 20]
+
+console.log ("show if some value of array is 7")
+var result= some;
+
+function some(array,callback) {
+    result = []
+    for (var i =0; i<array.lnegth; i++){
+    result = callback(array [i],[i])
+    }
+    return result
+
+   
+   
+}
