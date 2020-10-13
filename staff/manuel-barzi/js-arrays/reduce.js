cb4efc4eb1ffde5ea5e65a9818784e1b7657d75a
manuelbarzi <manuelbarzi@gmail.com>
2020-10-13 15:25:23 +0200

    add reduce

--- staff/manuel-barzi/js-arrays/reduce.js
+++ staff/manuel-barzi/js-arrays/reduce.js
@@ -1,14 +1,14 @@
 function reduce(array, callback, initialValue) {
     if (!(array instanceof Array)) throw new TypeError(array + ' is not an array')
     if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function')  
-    
+
     if (initialValue !== undefined) {
         var accum = initialValue
 
         for (var index = 0; index < array.length; index++) {
             var element = array[index]
 
-            accum = callback(accum, element)
+            accum = callback(accum, element, index, array)
         }
 
         return accum
@@ -18,7 +18,7 @@ function reduce(array, callback, initialValue) {
         for (var index = 1; index < array.length; index++) {
             var element = array[index]
 
-            accum = callback(accum, element)
+            accum = callback(accum, element, index, array)
         }
 
         return accum
