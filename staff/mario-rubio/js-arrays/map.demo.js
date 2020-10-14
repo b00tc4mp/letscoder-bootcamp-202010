41067021f65f6333e55e0dbf750f8c3b7f536bb5
MarioRubio81 <m.rubiogonzalez@gmail.com>
2020-10-14 18:09:25 +0200

    adding arrays

--- staff/mario-rubio/js-arrays/map.demo.js
+++ staff/mario-rubio/js-arrays/map.demo.js
@@ -2,11 +2,18 @@ console.log('DEMO map')
 
 var vals = [1,2,3,4,5,6,7,8,9];
 
+
+
 console.log('multiply numbers*3 in array', vals)
 
 var output = map (vals,function(value){return value*3});
 
 console.log (output);
 
+
+
+console.log('return de result of 10-each value of de array', vals)
+
 var hola =map (vals,function(value){return 10-value})
 console.log (hola)
+
