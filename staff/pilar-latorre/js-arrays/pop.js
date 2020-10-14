e2496031ce28ba3b011c67d0d1a004ae3585a485
Pilar <pilarlatorre82@gmail.com>
2020-10-14 20:58:40 +0200

    last update 14.10

---
+++ staff/pilar-latorre/js-arrays/pop.js
@@ -0,0 +1,14 @@
+var arr = ['salt', 'tomato', 'sugar', 'oil']
+
+
+function pop (array){
+
+   
+    for (var i=0; i< arr.length; i ++){
+
+
+
+    }
+
+
+}
---
+++ staff/pilar-latorre/js-arrays/slice.js
@@ -0,0 +1,13 @@
+var nombres = ['PEPE', 'JESUS', 'MARTINA','MARTIN']
+
+var result = []
+function slice (array, initial, limit){
+    for (var i = 0; i< limit; i++){
+        
+        result[i] = array[i]
+
+    }
+}
+
+slice (nombres,0,2)
+console.log(result)
