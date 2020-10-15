8dac69a1c96f5ffceea04bd4eb246b258d3a3ec8
LucasWloch <lucaswloch2019@gmail.com>
2020-10-15 22:10:43 +0200

    add error handling

--- staff/lucas-wloch/js-arrays/map.js
+++ staff/lucas-wloch/js-arrays/map.js
@@ -1,6 +1,6 @@
 function map(array, callback){
-    if (!(array instanceof Array)) throw new TypeError ( array + " is not an array")
-    if (typeof callback !== "function") throw new TypeError (callback + " is not a function")
+    if (!(array instanceof Array)) throw new TypeError ( typeof array + " is not an array")
+    if (typeof callback !== "function") throw new TypeError (typeof callback + " is not a function")
     // console.log(array[1])
     var newarr = []
     for(var i = 0; i<array.length; i++){
@@ -12,9 +12,9 @@ function map(array, callback){
 }
 
 
-var arr = [1, 2, 3, 4, 5]
+// var arr = [1, 2, 3, 4, 5]
 
-var result = map(arr,function(value){
-    return value + value;
-});
-console.log(result)
+// var result = map(arr,function(value){
+//     return value + value;
+// });
+// console.log(result)
--- staff/lucas-wloch/js-arrays/slice.js
+++ staff/lucas-wloch/js-arrays/slice.js
@@ -1,5 +1,14 @@
-function slice(array, initial, limit){
+function slice(array, start, end){
     var result = []
+   
+    if (start === undefined) start = 0
+    if (start < 0) start = array.length + start //si start es negativo 
+    //le pasamos a start la longitud del array menos start
+    if (end === undefined) end = array.length
+
+    if (end < 0) end = array.length + end //si end es negativo es para 
+    //restemos los ulttimos items entoonces le end a la cant de iteraciones del for
+
     for(var i=initial; i<limit ;i++){
         
         result[result.length] = array[i]
