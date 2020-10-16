2689bd821f6d22de4d4948f4a5f35a346b4c002f
manuelbarzi <manuelbarzi@gmail.com>
2020-10-16 18:10:21 +0200

    boost caray

--- staff/manuel-barzi/caray/caray.test.js
+++ staff/manuel-barzi/caray/caray.test.js
@@ -1,6 +1,6 @@
 console.log('TEST Caray (constructor)');
 
-(function() {
+(function () {
     console.log(' should create a Caray of length 10 when it receives only that argument')
 
     var c = new Caray(10)
@@ -11,14 +11,14 @@ console.log('TEST Caray (constructor)');
         console.assert(c[i] === undefined, 'should item at index ' + i + ' be undefined')
 })();
 
-(function() {
+(function () {
     console.log(' should fail when it receives only one numeric non-integer argument')
 
     var fail
 
     try {
         var c = new Caray(10.10)
-    } catch(error) {
+    } catch (error) {
         fail = error
     }
 
@@ -27,13 +27,15 @@ console.log('TEST Caray (constructor)');
     console.assert(fail.message === 'Invalid array length', 'should error message match')
 })();
 
-(function() {
+(function () {
     console.log(' should create a Caray of length 1 when it receives only one argument non-numeric and non-integer')
 
-    var c = new Caray('hello world')
+    var value = ['hello world', true, null, undefined, { hello: 'world' }, ['hello', 'world']].random()
+
+    var c = new Caray(value)
 
     console.assert(c.length === 1, 'should length be 1')
-    console.assert(c[0] === 'hello world', 'should first item match')
+    console.assert(c[0] === value, 'should first item match')
 })();
 
 // TODO test constructor for multiple arguments
