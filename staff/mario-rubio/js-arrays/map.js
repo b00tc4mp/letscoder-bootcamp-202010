9a57b2871d91b0497c81c361bc83d820eef8af01
MarioRubio81 <m.rubiogonzalez@gmail.com>
2020-10-16 13:58:26 +0200

    changes

--- staff/mario-rubio/js-arrays/map.js
+++ staff/mario-rubio/js-arrays/map.js
@@ -1,11 +1,11 @@
-function map(arr, expression){
-    if (!(arr instanceof Array)) throw new TypeError ( arr + " is not an array")
+function map(array, expression){
+    if (!(array instanceof Array)) throw new TypeError ( array + " is not an array")
     if (typeof expression !== "function") throw new TypeError (expression + " is not a function")
     // console.log(arr[1])
     var newarr = []
-    for(var i = 0; i<arr.length; i++){
+    for(var i = 0; i<array.length; i++){
         // console.log(arr[i])
-        newarr.push(expression(arr[i]))
+        newarr.push(expression(array[i]))
 
     }
     return newarr
