f48163be070302d58de373cc82311f42a6ed35b8
Nicolas <nicolaspostigoausere@hotmail.es>
2020-10-17 15:48:48 +0200

    cambios

--- staff/nicolas-postigo/caray/caray-filter.test.js
+++ staff/nicolas-postigo/caray/caray-filter.test.js
@@ -1,20 +1,43 @@
-console.log("TEST Caray.prototype.filter()")
+console.log('TEST Caray.prototype.filter()');
 
 (function(){
-    console.log(" should put in a new Caray all the words that have length bigger tahn 6");
+console.log (" should put in a new caray words with length bigger than 6");
 
 var months = new Caray ("January", "February", "March", "April", "May", "June");
 
 var iterations = 0;
 
-var result = months.filter(function(value){
-    iterations ++;
+var result = months.filter(function (value){
+    iterations++
 
-    return value.length > +;
+    return value.length>6;
 })
 
-console.assert(result[0] === "January", "should the result[0] be January")
-console.assert(result[1] === "February", "should be the result[1] be Febraury")
-console.assert(result.length === 2, "should the result length be 2")
-console.assert(months.lenght === 6, "sh")
-})
+console.assert(result[0]=== "January", "Shoul the result[0] be January");
+console.assert(result[1]=== "February", "Shoul the result[1] be February");
+console.assert(result.length === 2, "shoul the resul length de 2");
+console.assert(months.length === 6, "should the months be 6")
+console.assert(iterations === 6, "should the iterations de 6");
+
+})();
+
+
+(function(){
+console.log (" should throw an error if callback is not a function");
+
+    var months = new Caray ("January", "February", "March", "April", "May", "June");
+
+    var fail
+
+    try {
+        months.filter("a")
+        
+    } catch (error) {
+        fail = error;
+    }
+console.assert(fail instanceof TypeError, "should be TypeError");
+console.assert(fail.message === "a is not a function","the error message is not correct");
+
+
+
+})();
--- staff/nicolas-postigo/caray/caray-some.test.js
+++ staff/nicolas-postigo/caray/caray-some.test.js
@@ -22,14 +22,30 @@ console.assert(iterations === 3, 'should iterations count be 3')
 (function(){
     console.log(" should trhow an error if callback  s not a fuction")
 
-    var weather = new Caray ("rain", "sun", "wind");
+var weather = new Caray ("rain", "sun", "wind");
 
-    var fail;
+var fail;
 
-    try {
-        weather.some("pepito");
+try {
+    weather.some("pepito");
     } catch (error) {
         fail = error;
     }
 
     })();
+
+
+    Caray.prototype.filter = function filter(callback) {
+    
+        if (typeof callback !== "function") throw new TypeError (callback + " is not a function")
+        
+        var result = new Caray
+    
+        for (var i = 0; i < this.length; i++) {
+            var value = this[i]
+    
+            if (callback(value)) result.push(value)
+        }
+    
+        return result
+    }
