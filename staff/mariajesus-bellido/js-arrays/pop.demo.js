ad6e8e4f93b773b215c53da10c28b74d4f8917c4
mariajbellido <54066498+mariajbellido@users.noreply.github.com>
2020-10-19 00:49:28 +0200

    adding pop

---
+++ staff/mariajesus-bellido/js-arrays/pop.demo.js
@@ -0,0 +1,21 @@
+console.log("Demo for POP"); 
+
+
+var colours = ["black", "blue", "green", "yellow"]; 
+
+console.log("Remove the last element of this array colours ", colours); 
+
+pop(colours); 
+console.log(colours); 
+
+
+console.log("Otro ejemplo ----------"); 
+
+
+var cities = ["Barcelona", "Madrid", "Valencia"]; 
+
+console.log("Remove the last element of the array cities", cities); 
+
+pop(cities); 
+
+console.log(cities); 
