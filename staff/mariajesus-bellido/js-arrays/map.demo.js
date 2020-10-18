e78db25b3dc3b3709f256498d6c843c281bf9833
mariajbellido <54066498+mariajbellido@users.noreply.github.com>
2020-10-19 01:01:22 +0200

    adding map, reduce and renaming some files

--- staff/mariajesus-bellido/js-arrays/map.demo.js
+++ staff/mariajesus-bellido/js-arrays/map.demo.js
@@ -1,7 +1,43 @@
+console.log("DEMO for MAP"); 
+
+var nums = [1, 2, 3, 4, 5]; 
+
+console.log("multiply numbers by 10 from ", nums); 
+
+var result = map(nums, function(value) {return value * 10})
+
+console.log(result); 
+
+console.log("Convert numbers into strings from ", nums); 
+
+
+var result = map(nums, function(value)  { return value + ''}); 
+
+console.log(result); 
+
+
+
+var booleans = [true, false, false, true]; 
+
+console.log ("convert these booleans to string and then change them to uppercase from", booleans);
+
+var result = map(booleans, function(value)  {return (value + ' ' ).toUpperCase()})
+
+
+console.log (result); 
+
+
+
+
+
+
+console.log("An additional demo / example with sort of common data"); 
+
+
 console.log("We have a group of companies, and we want to get the company names")
 
 
-const companies= [
+var companies= [
     {name: "Company One", category: "Finance", start: 1981, end: 2004},
     {name: "Company Two", category: "Retail", start: 1992, end: 2008},
     {name: "Company Three", category: "Auto", start: 1999, end: 2007},
@@ -13,11 +49,10 @@ const companies= [
     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
   ];
   
-  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+var ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
   
   // Get company names 
-
-  const companyNames = companies.map (function(company) {
+var companyNames = companies.map (function(company) {
   
     return company.name;}); 
   
