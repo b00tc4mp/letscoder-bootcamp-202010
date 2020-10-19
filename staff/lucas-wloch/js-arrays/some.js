5ee25e8ad143b8a284673275e2655900cbc09507
LucasWloch <lucaswloch2019@gmail.com>
2020-10-19 15:26:22 +0200

    add caray tests

--- staff/lucas-wloch/js-arrays/some.js
+++ staff/lucas-wloch/js-arrays/some.js
@@ -1,7 +1,7 @@
-function some(arr, expression){
-    for (var i = 0; i < arr.length; i++) {
+function some(array, callback){
+    for (var i = 0; i < array.length; i++) {
         
-        if (expression(arr[i])) return true
+        if (callback(array[i])) return true
     }
     return false
 }
