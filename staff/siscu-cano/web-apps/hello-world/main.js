db89a24c4c64ec4ae5f20aae14e2fc65bc177ed2
Siscu Cano <siscuca@gmail.com>
2020-10-20 21:48:38 +0200

    Add logic test in login

--- staff/siscu-cano/web-apps/hello-world/main.js
+++ staff/siscu-cano/web-apps/hello-world/main.js
@@ -73,20 +73,10 @@
         var app = document.querySelector('.web-app');
         event.preventDefault();
 
-        if (!email.trim().length) throw new Error('e-mail is empty or blank')
-        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) throw new Error('invalid e-mail')
+        authenticateUser(email, password);
 
-        var userFound = users.find(
-            function(user) {
-                return user.email === email && user.password === password;
-            });
-
-        if (userFound) {
-            login.classList.add('hidden')
-            app.classList.remove('hidden');
-        } else {
-            throw new Error('Email not registered or password is wrong!');
-        }
+        login.classList.add('hidden');
+        app.classList.remove('hidden');
     }
 
 })();
