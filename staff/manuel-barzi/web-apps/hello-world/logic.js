ab910d6b72748afb399fc57c63e8af3e3c763dd3
manuelbarzi <manuelbarzi@gmail.com>
2020-10-20 16:39:24 +0200

    boost hello world app logic testing

--- staff/manuel-barzi/web-apps/hello-world/logic.js
+++ staff/manuel-barzi/web-apps/hello-world/logic.js
@@ -1,15 +1,23 @@
 function registerUser(fullname, email, password, repassword) {
+    if (typeof fullname !== 'string') throw new TypeError(fullname + ' is not a full name')
+
     if (!fullname.trim().length) throw new Error('full name is empty or blank')
+
+    if (typeof email !== 'string') throw new TypeError(email + ' is not an e-mail')
 
     if (!email.trim().length) throw new Error('e-mail is empty or blank')
 
     if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) throw new Error('invalid e-mail')
 
+    if (typeof password !== 'string') throw new TypeError(password + ' is not a password')
+
     if (!password.trim().length) throw new Error('password is empty or blank')
 
-    if (!repassword.trim().length) throw new Error('repeat password is empty or blank')
+    if (typeof repassword !== 'string') throw new TypeError(repassword + ' is not a password repeat')
 
-    if (password !== repassword) throw new Error('passwords do not match')
+    if (!repassword.trim().length) throw new Error('password repeat is empty or blank')
+
+    if (password !== repassword) throw new Error('passwords don\'t match')
 
     var user = users.find(function (user) {
         return user.email === email
