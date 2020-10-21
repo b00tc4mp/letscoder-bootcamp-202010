b03346f6d057090a29c673148e8175ea914b9315
BetoDav <betodav@BetoDavs-MacBook-Pro.local>
2020-10-21 09:59:45 +0200

    refactoring web-apps

---
+++ staff/beto-davila/web-apps/hello-world/main.js
@@ -0,0 +1,127 @@
+
+// Back to home page
+(function () {
+  var home = document.querySelector('.home')
+  var sections = document.querySelectorAll("section");
+  var title = document.querySelector("h1");
+
+  title.onclick = function () {
+    for (var i = 0; i < sections.length; i++) {
+      sections[i].classList.add("off");
+      home.classList.remove("off");
+    }
+  }
+
+})();
+
+// Home Page register or login
+(function () {
+
+  var home = document.querySelector('.home');  
+  var register = home.querySelector('.home__register');
+  var login = home.querySelector('.home__login');
+
+  register.onclick = function () {
+    home.classList.add('off');
+    var register = document.querySelector('.register');
+    register.classList.remove('off');
+  }
+
+  login.onclick = function () {
+      home.classList.add('off');
+      var login = document.querySelector('.login');
+      login.classList.remove('off');
+  }
+
+})();
+
+
+// Register form
+
+(function () {
+  var login = document.querySelector('.login');
+  var register = document.querySelector('.register');
+  var formRegister = register.querySelector('.register__form');
+  var formLogin = login.querySelector('.login__form');
+  
+
+  formRegister.onsubmit = function (event) {
+    event.preventDefault(); // Clicking on the submit button, prevents it from submitting the form by default
+
+    var inputs = document.querySelectorAll("input");
+    var fullname = inputs[0].value;
+    var email = inputs[1].value;
+    var password = inputs[2].value;
+    var repassword = inputs[3].value;
+
+    registerUser(fullname, email, password, repassword);
+
+    var register = document.querySelector('.register');
+
+    register.classList.add("off");
+
+    var confirm = document.querySelector('.login');
+
+    confirm.classList.remove("off");
+
+    var message = formLogin.querySelector('.login__message');
+
+    message.classList.remove("off");
+  };
+
+})();
+
+// register confirmation
+
+(function () {
+  var login = document.querySelector('.login');
+  var form = login.querySelector('.login__form');
+  var loginBtn = form.querySelector('.login__btn');
+
+  loginBtn.onclick = function () {
+    var confirm = document.querySelector('.login');
+
+    confirm.classList.add("off");
+
+    var welcome = document.querySelector('.welcome');
+    welcome.classList.remove("off");
+  };
+})();
+
+
+// Login form
+(function () {
+  var login = document.querySelector('.login__btn');
+
+  login.onclick = function () {
+    var loginScreen = document.querySelector('.login');
+
+    loginScreen.classList.remove('off');
+
+    var home = document.querySelector('.home');
+
+    home.classList.add('off');
+  };
+})();
+
+// Login submit
+
+(function() {
+
+    var login = document.querySelector('.login__form');
+    var loginSection = document.querySelector('.login');
+    var welcome = document.querySelector('.welcome');
+    
+    login.onsubmit = function(event) {
+        event.preventDefault();
+        var input = document.querySelectorAll('input');
+        var email = input[4].value;
+        var password = input[5].value;
+        
+        authenticateUser(email, password);
+
+        welcome.classList.remove('off');
+        loginSection.classList.add('off');      
+    }
+
+})(); 
