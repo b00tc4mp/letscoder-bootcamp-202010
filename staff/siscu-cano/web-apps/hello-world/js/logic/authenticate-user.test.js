8f12b7a157bef294fc057f365b02edb0f9e3f282
Siscu Cano <siscuca@gmail.com>
2020-10-24 12:19:48 +0200

    Add authenticate test

--- staff/siscu-cano/web-apps/hello-world/js/logic/authenticate-user.test.js
+++ staff/siscu-cano/web-apps/hello-world/js/logic/authenticate-user.test.js
@@ -2,12 +2,10 @@
     console.log('TEST authenticateUser()');
 
     (function () {
-        console.log(' should succeed on existing user')
+        console.log(' should authenticate if the user we tested already existed (Test1)')
         var fullname = 'John Mama ' + Math.random()
         var email = 'johnmama-' + Math.random() + '@mail.com'
         var password = 'pass-' + Math.random()
-
-        console.log(' should succeed on new user')
 
         call('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users',
             { 'Content-type': 'application/json' },
@@ -25,8 +23,11 @@
                             },
                             '{ "password": "' + password + '" }',
                             function (status, response) {
-                                console.assert(status === 204, 'should status be 204')
-                                console.assert(response.length === 0, 'should response be empty')
+                                if (status === 204) {
+                                    console.log(' \t Deleted the user registered for the test (Test1)')   
+                                } else {
+                                    console.log('Could not delete the user registered for the test (Test1)')
+                                }
                             }
                         )
                     })
@@ -37,25 +38,19 @@
     })();
 
     (function () {
-        console.log(' should fail on non-existing user')
+        console.log(' should fail on non-existing user (Test2)')
 
         var email = 'johndoe-' + Math.random() + '@mail.com'
         var password = 'pass-' + Math.random()
 
-        var fail
-
-        try {
-            authenticateUser(email, password)
-        } catch(error) {
-            fail = error
-        }
-
-        console.assert(fail instanceof Error, 'should error be defined and instance of Error')
-        console.assert(fail.message, 'wrong credentials')
+        authenticateUser(email, password, function (error, token) {
+            console.assert(error instanceof Error, 'should error be defined and instance of Error')
+            console.assert(error.message, 'username and/or password wrong')
+        })
     })();
 
     (function () {
-        console.log(' should fail on wrong password')
+        console.log(' should fail on wrong password (Test3)')
 
         var fullname = 'John lol ' + Math.random()
         var email = 'johnlol-' + Math.random() + '@mail.com'
@@ -83,8 +78,11 @@
                                     },
                                     '{ "password": "' + password + '" }',
                                     function (status, response) {
-                                        console.assert(status === 204, 'should status be 204')
-                                        console.assert(response.length === 0, 'should response be empty')
+                                        if (status === 204) {
+                                            console.log(' \t Deleted the user registered for the test (Test3)')   
+                                        } else {
+                                            console.log('Could not delete the user registered for the test (Test3)')
+                                        }
                                     }
                                     )
                             }
@@ -101,34 +99,56 @@
     })();
 
     (function () {
-        console.log(' should fail on wrong email')
+        console.log(' should fail on wrong email (Test4)')
 
         var fullname = 'John Doe ' + Math.random()
         var email = 'johndoe-' + Math.random() + '@mail.com'
         var password = 'pass-' + Math.random()
 
-        var user = {
-            fullname: fullname,
-            email: email,
-            password: password
-        }
-
-        users.push(user)
-
-        var fail
-
-        try {
-            authenticateUser('wrong-' + email, password)
-        } catch(error) {
-            fail = error
-        }
-
-        console.assert(fail instanceof Error, 'should error be defined and instance of Error')
-        console.assert(fail.message, 'wrong credentials')
+        call('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users',
+        { 'Content-type': 'application/json' },
+        '{ "fullname": "' + fullname + '", "username": "' + email + '", "password": "' + password + '" }',
+        function (status, response) {
+            if (status === 201) {
+                authenticateUser('email@notexist.com', password, function (error, token) {
+                    console.assert(error instanceof Error, 'should error be defined and instance of Error')
+                    console.assert(error.message, 'username and/or password wrong')
+                    call('POST',
+                    'https://b00tc4mp.herokuapp.com/api/v2/users/auth',
+                    { 'Content-type': 'application/json' },
+                    '{ "username": "' + email + '", "password": "' + password + '" }',
+                    function (status, response) {
+                        if (status === 200) {
+                            var res = JSON.parse(response)
+                            call('DELETE', 'https://b00tc4mp.herokuapp.com/api/v2/users',
+                                {
+                                    'Authorization': 'Bearer ' + res.token,
+                                    'Content-type': 'application/json'
+                                },
+                                '{ "password": "' + password + '" }',
+                                function (status, response) {
+                                    if (status === 204) {
+                                        console.log(' \t Deleted the user registered for the test (Test4)')   
+                                    } else {
+                                        console.log('Could not delete the user registered for the test (Test4)')
+                                    }
+                                }
+                                )
+                        }
+                        else {
+                            console.error('should not reach this point')
+                        }
+                    }
+                )
+                })
+            } else {
+                console.error('should not reach this point')
+            }
+    })
     })();
 
     (function () {
-        console.log(' should fail on non-string email')
+        console.log(' should fail on non-string email (Test5)')
 
         var email = [1, true, null, undefined, {}, [], function () { }, new Date].random()
         var password = 'pass-' + Math.random()
@@ -140,13 +160,12 @@
         } catch (error) {
             fail = error
         }
-
         console.assert(fail instanceof TypeError, 'should error be defined and an instance of TypeError')
         console.assert(fail.message === email + ' is not an e-mail', 'should error message match expected')
     })();
 
     (function () {
-        console.log(' should fail on empty or blank email')
+        console.log(' should fail on empty or blank email (Test 6)')
 
         var email = ['', ' ', '\t', '\n'].random()
         var password = 'pass-' + Math.random()
@@ -158,13 +177,12 @@
         } catch (error) {
             fail = error
         }
-
         console.assert(fail instanceof Error, 'should error be defined and an instance of TypeError')
-        console.assert(fail.message === 'e-mail is empty or blank', 'should error message match expected')
+        console.assert(fail.message === email + ' is empty or blank', 'should error message match expected')
     })();
 
     (function () {
-        console.log(' should fail invalid email')
+        console.log(' should fail invalid email (Test7)')
 
         var email = ['john-doe#mail.com', '@mail.com', 'johh-doe@mail', 'john-doe@'].random()
         var password = 'pass-' + Math.random()
@@ -182,7 +200,7 @@
     })();
 
     (function () {
-        console.log(' should fail on non-string password')
+        console.log(' should fail on non-string password (Test8)')
 
         var email = 'johndoe-' + Math.random() + '@mail.com'
         var password = [1, true, null, undefined, {}, [], function () { }, new Date].random()
@@ -200,7 +218,7 @@
     })();
 
     (function () {
-        console.log(' should fail on empty or blank password')
+        console.log(' should fail on empty or blank password (Test9)')
 
         var email = 'johndoe-' + Math.random() + '@mail.com'
         var password = ['', ' ', '\t', '\n'].random()
@@ -212,9 +230,8 @@
         } catch (error) {
             fail = error
         }
-
         console.assert(fail instanceof Error, 'should error be defined and an instance of TypeError')
-        console.assert(fail.message === 'password is empty or blank', 'should error message match expected')
+        console.assert(fail.message === password + ' is empty or blank', 'should error message match expected')
     })();
 
 })();
