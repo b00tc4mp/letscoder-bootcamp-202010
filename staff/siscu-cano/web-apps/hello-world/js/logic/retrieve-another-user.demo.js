c12f16a0a71b4672b45a94223449329f79d8658e
Siscu Cano <siscuca@gmail.com>
2020-10-25 09:28:56 +0100

    Retrieve another user demo

--- staff/siscu-cano/web-apps/hello-world/js/logic/retrieve-another-user.demo.js
+++ staff/siscu-cano/web-apps/hello-world/js/logic/retrieve-another-user.demo.js
@@ -1,1 +1,6 @@
-// TODO play here
+retrieveAnotherUser('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Zjk1MjFjMzk1ZGUxODAwMTcxYmIxZjAiLCJpYXQiOjE2MDM2MTQzOTAsImV4cCI6MTYwMzYxNzk5MH0.uAK-0Hp3nXxiXjILptWCzLpokKq7Cq_19y3GcPfuptM','5f9151872d217c00176d8b71', function(error, user) {
+    console.log('DEMO retriveUser()')
+
+    if (error) console.error(error)
+    else console.dir(user)
+})
