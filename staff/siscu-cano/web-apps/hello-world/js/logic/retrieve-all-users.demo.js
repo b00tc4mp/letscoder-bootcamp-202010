ef3b8a83ce968411c67ca552d36eebf9a9e62e1e
Siscu Cano <siscuca@gmail.com>
2020-10-25 08:34:18 +0100

    Retrieve all users demo

--- staff/siscu-cano/web-apps/hello-world/js/logic/retrieve-all-users.demo.js
+++ staff/siscu-cano/web-apps/hello-world/js/logic/retrieve-all-users.demo.js
@@ -1,1 +1,6 @@
-// TODO play here
+retrieveAllUsers('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Zjk0OWRhY2I1ZmFlNTAwMTdmNTM3MDciLCJpYXQiOjE2MDM2MDc1NDksImV4cCI6MTYwMzYxMTE0OX0.OVOmO3H3yjIG-Z5Pgk4hDKG4MERCMdEX4RVze4MQPjs', function(error, user) {
+    console.log('DEMO retriveAllUser()')
+
+    if (error) console.error(error)
+    else console.log(user)
+})
