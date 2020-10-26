4a42af6d4cb9fa61a2ac52d6ad787cf9a9d729a7
Cristian Manzano Rodriguez <CRIS@MacBook-Pro-de-Cristian.local>
2020-10-26 15:44:29 +0100

    remove hello-world-react

--- staff/cristian-manzano/web-apps/hello-world-react/components/access.demo.jsx
+++
@@ -1,5 +0,0 @@
-ReactDOM.render(
-    <Access onRegister={function() { console.log('go to register')}}
-        onLogin={function() { console.log('go to login')}} />,
-        document.getElementById('root')
-)
--- staff/cristian-manzano/web-apps/hello-world-react/components/access.jsx
+++
@@ -1,5 +0,0 @@
-function Access(props) {
-    return <section className="access">
-        <button className="access__register" onClick={props.onRegister}>Register</button> or <button className="access__login" onClick={props.onLogin}>Login</button>
-    </section>
-}
