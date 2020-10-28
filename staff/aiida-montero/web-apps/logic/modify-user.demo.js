fb347ebd6ea24a9e11841714f305846642a7971b
Aida Montero <aidacastro1996@gmail.com>
2020-10-28 18:01:25 +0100

    add changes about logic

--- staff/aiida-montero/web-apps/logic/modify-user.demo.js
+++ staff/aiida-montero/web-apps/logic/modify-user.demo.js
@@ -1,4 +1,4 @@
-modifyUser({"hair": "black"},'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Zjk0OWQ4MWI1ZmFlNTAwMTdmNTM3MDYiLCJpYXQiOjE2MDM2Mzg1NDcsImV4cCI6MTYwMzY0MjE0N30.JGF6yMZBxmRuA-4nXBsNHU6-e9XnksBjjcQOlJam3gk',function(error) {
+modifyUser({"hair": "black"},'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Zjk5OGViMmQwODAwYjAwMTcwNTMwNzIiLCJpYXQiOjE2MDM4OTkwNjAsImV4cCI6MTYwMzkwMjY2MH0.2nnimYTpDO8QSwCbHS4itIc4Anw-3YQI1iGNnELVSV0',function(error) {
     console.log ('DEMO modifyuser()')
     if(!error)console.log('add changes')
     else console.error ('ERROR!'+ error.message)
