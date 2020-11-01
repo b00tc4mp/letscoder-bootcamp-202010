ee5308730f0e9d2eea06ee628a6ba38fb85c5094
Aida Montero <aidacastro1996@gmail.com>
2020-11-01 18:25:18 +0100

    add test register user

--- staff/groups/the-movie-records/movie-app/js/logic/test/register-user.spec.js
+++ staff/groups/the-movie-records/movie-app/js/logic/test/register-user.spec.js
@@ -11,9 +11,9 @@ describe("SPEC registerUser()", function () {
       repassword = password;
     });
 
-    it("should succeed on right token", function (done) {
+    it("should succeed on new register", function (done) {
       registerUser(fullname, email, password, repassword, function (error) {
-        expect(error).toEqual(null);
+        expect(error).toBeNull();
         done();
       });
     });
@@ -74,4 +74,296 @@ describe("SPEC registerUser()", function () {
     });
   });
   //------------------------------------------------------------------------------------------
+
+  describe("when user already exists", function () {
+    let fullname, email, password, repassword;
+
+    beforeEach(function (done) { 
+     fullname = 'John Doe ' + random()
+     email = 'johndoe-' + random() + '@mail.com'
+     password = 'pass-' + random()
+     repassword = password
+     call('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users',
+     { 'Content-type': 'application/json' },
+     '{ "fullname": "' + fullname + '", "username": "' + email + '", "password": "' + password + '" }',
+     function (status, response) {
+         expect(status).toBe(201)
+         expect(response.length).toBe(0)
+         done()
+      }) 
+    });
+     
+    it("should fail on already existing user", function (done) {
+      registerUser(fullname, email, password, repassword, function (error) {
+        expect(error).toBeInstanceOf(Error)
+        expect(error.message).toBe(`user with username "${email}" already exists`) 
+        
+        done()
+      })
+
+    });
+   
+    afterEach(function (done) 
+    {call('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users/auth', { 'Content-type': 'application/json' },
+    '{ "username": "' + email + '", "password" : "' + password + '" }',
+    function (status, response) {
+        expect(status).toBe(200)
+        
+        var res = JSON.parse(response)
+        
+        var token = res.token
+        console.log(token)
+
+        call('DELETE', 'https://b00tc4mp.herokuapp.com/api/v2/users',
+            {
+                'Authorization': 'Bearer ' + token,
+                'Content-type': 'application/json'
+            },
+            '{ "password": "' + password + '" }',
+            function (status, response) {
+                expect(status).toBe(204)
+                expect(response.length).toBe(0)
+                done()
+            }
+        )
+    }
+)});
+
+
+    
+
+  });
+  //----------------------------------------------------------------------------------------------
+  describe("when fullname is not a string" , function () {
+    let fullname, email, password;
+
+    beforeEach(function () {
+          fullname = [1, true, null, undefined, {}, [], function () { }, new Date].random()
+          email = 'johndoe-' + random() + '@mail.com'
+          password = 'pass-' + random()
+          repassword = password
+    });
+
+    it("should fail on non string fullname", function () {
+         expect(function () {
+         registerUser(fullname, email, password, repassword, function () {});
+      }).toThrowError(TypeError, `${fullname} is not a full name`);
+    });
+
+  });
+
+  //-----------------------------------------------------------------------------------------------
+  describe("when fullname is empty or blank", function () {
+    let fullname, email, password, repassword ;
+
+    beforeEach(function () {
+        fullname = ['', ' ', '\t', '\n'].random()
+        email = 'johndoe-' + random() + '@mail.com'
+        password = 'pass-' + random()
+        repassword = password
+
+    });
+
+    it("should be error on empty or blank fullname", function () 
+     {
+      expect(function () {
+        registerUser(fullname, email, password, repassword, function () {});
+      }).toThrowError(Error, `full name is empty or blank`);
+    });
+
+    afterEach(function () {});
+  });
+
+  //------------------------------------------------------------------------------------------------
+  describe("when e-mail is not a string", function () {
+    let fullname, email, password, repassword;
+
+    beforeEach(function () 
+    {
+         fullname = 'John Doe ' + random()
+         email = [1, true, null, undefined, {}, [], function () { }, new Date].random()
+         password = 'pass-' + random()
+         repassword = password
+    });
+
+    it("should be error on non string e-mail", function () {
+        expect(function () {
+        registerUser(fullname, email, password, repassword, function () {});
+      }).toThrowError(TypeError, `${email} is not an e-mail`);
+    });
+
+   
+  });
+  //-------------------------------------------------------------------------------------------------
+
+  describe("when e-mail is empty or blank ", function () {
+    let fullname, email, password, repassword;
+
+    beforeEach(function () { 
+    fullname = 'John Doe ' + random()
+     email = ['', ' ', '\t', '\n'].random()
+     password = 'pass-' + random()
+     repassword = password
+
+  });
+
+    it("should fail on empty or blank e-mail", function () {
+         expect(function () {
+        registerUser(fullname, email, password, repassword, function () {});
+      }).toThrowError(Error, `e-mail is empty or blank`);
+    });
+
+  });
+
+   //--------------------------------------------------------------------------------------------------
+  
+
+   describe("when e-mail is invalid", function () {
+    let fullname, email, password, repassword;
+
+    beforeEach(function () {
+       fullname = 'John Doe ' + Math.random()
+       email = ['john-doe#mail.com', '@mail.com', 'johh-doe@mail', 'john-doe@'].random()
+       password = 'pass-' + Math.random()
+       repassword = password
+    });
+
+    it("should fail in ivalid e-mail", function () {
+        expect(function () {
+        registerUser(fullname, email, password, repassword, function () {});
+      }).toThrowError(Error, `invalid e-mail`);
+
+    });
+
+
+  });
+  //-----------------------------------------------------------------------------------------------------
+  describe("when password is not a string", function () {
+    let fullname, email, password, repassword;
+
+    beforeEach(function () {
+        fullname = 'John Doe ' + random()
+         email = 'johndoe-' + random() + '@mail.com'
+         password = [1, true, null, undefined, {}, [], function () { }, new Date].random()
+         repassword = 'pass-' + random()
+      
+    });
+
+    it("should fail on non string password", function () {
+        expect(function () {
+        registerUser(fullname, email, password, repassword, function () {});
+      }).toThrowError(TypeError, `${password} is not a password`);
+
+    });
+
+
+  });
+  //------------------------------------------------------------------------------------------------------
+  describe("when password is empty or blank", function () {
+    let fullname, email, password, repassword;
+
+    beforeEach(function () {
+      fullname = 'John Doe ' + random()
+      email = 'johndoe-' + random() + '@mail.com'
+      password = ['', ' ', '\t', '\n'].random()
+      repassword = 'pass-' + random()
+      
+    });
+
+    it("should fail on empty or blank password", function () {
+        expect(function () {
+        registerUser(fullname, email, password, repassword, function () {});
+      }).toThrowError(Error, `password is empty or blank`);
+
+    });
+
+
+  });
+  //------------------------------------------------------------------------------------------------------
+  describe("when repassword is non a string", function () {
+    let fullname, email, password, repassword;
+
+    beforeEach(function () {
+      fullname = 'John Doe ' + random()
+      email = 'johndoe-' + random() + '@mail.com'
+      password = 'pass-' + random()
+      repassword = [1, true, null, undefined, {}, [], function () { }, new Date].random()
+      
+    });
+
+    it("should fail on repassword is not a string", function () {
+        expect(function () {
+        registerUser(fullname, email, password, repassword, function () {});
+      }).toThrowError(TypeError, `${repassword} is not a password repeat`);
+
+    });
+
+
+  });
+  //-------------------------------------------------------------------------------------------------------
+  describe("when repassword is empty or blank", function () {
+    let fullname, email, password, repassword;
+
+    beforeEach(function () {
+       fullname = 'John Doe ' + random()
+       email = 'johndoe-' + random() + '@mail.com'
+       password = 'pass-' + random()
+       repassword = ['', ' ', '\t', '\n'].random()
+      
+    });
+
+    it("should fail on repassword is empty or blank", function () {
+        expect(function () {
+        registerUser(fullname, email, password, repassword, function () {});
+      }).toThrowError(Error, `password repeat is empty or blank`);
+
+    });
+
+
+  });
+
+  //-------------------------------------------------------------------------------------------------------
+  describe("when mot matching password with repassword", function () {
+    let fullname, email, password, repassword;
+
+    beforeEach(function () {
+       fullname = 'John Doe ' + random()
+       email = 'johndoe-' + random() + '@mail.com'
+       password = 'pass-' + random()
+       repassword = password + '...'
+    });
+
+    it("should fail on not matching passwords", function () {
+        expect(function () {
+        registerUser(fullname, email, password, repassword, function () {});
+      }).toThrowError(Error, `passwords don't match`);
+
+    });
+
+
+  });
+  //--------------------------------------------------------------------------------------------------------
+
+  describe("when callback is not a function", function () {
+    let fullname, email, password, repassword, callback;
+
+    beforeEach(function () {
+         fullname = 'John Doe ' + random()
+         email = 'johndoe-' + random() + '@mail.com'
+         password = 'pass-' + random()
+         repassword = password
+         callback = [1, true, null, undefined, {}, [], 'string', new Date].random()
+    });
+
+    it("should fail on not function callback", function () {
+        expect(function () {
+        registerUser(fullname, email, password, repassword, callback);
+      }).toThrowError(TypeError, `${callback} is not a callback`);
+
+    });
+
+
+  });
 });
+
+
