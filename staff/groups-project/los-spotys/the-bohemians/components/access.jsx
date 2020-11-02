425addc841fcec7953c673e022a352d86273ea75
Cristian Manzano Rodriguez <CRIS@MacBook-Pro-de-Cristian.local>
2020-11-02 10:21:15 +0100

    removing wrong files from the branch

--- staff/groups-project/los-spotys/the-bohemians/components/access.jsx
+++
@@ -1,5 +0,0 @@
-function Access({ onRegister, onLogin }) {
-    return <section className="access">
-    <div className="button button__register" onClick={onRegister}>Register</div><div className="button button__login" onClick={onLogin}>Login</div>
-</section>
-}
