11030c83d66e1d0f76559bb758c2d1a31229a063
Aida Montero <aidacastro1996@gmail.com>
2020-11-03 10:02:48 +0100

    add slider react

--- staff/groups/the-movie-records/movie-app/js/components/slider.jsx
+++ staff/groups/the-movie-records/movie-app/js/components/slider.jsx
@@ -1,48 +1,24 @@
 
-function Slider (props){
+function Slider(props) {
 
   let posters = props && props.items && props.items.results && props.items.results.map(({ poster_path: image }) => {
     return { image };
   });
 
-   return(
-     
-     <div className="u-box">
-    <h1>{props.title}</h1>
-    <div className="scrollable-carousel">
+  return (
+
+    <div className="u-box">
+      <h1>{props.title}</h1>
+      <div className="scrollable-carousel">
 
 
-   {posters && posters.map(poster => { 
-    return <div className = "scrollable-carousel__item"> 
-              <img width='200' height='200' 
-              src={`https://image.tmdb.org/t/p/original${poster.image}`}></img>
-           </div>})}
-
-      <div className="scrollable-carousel__item">
-        <img
-          src="https://pics.filmaffinity.com/Bloodshot-229984487-large.jpg"
-          alt=""
-        />
-      </div>
-      <div className="scrollable-carousel__item">
-        <img
-          src="https://pics.filmaffinity.com/Bloodshot-229984487-large.jpg"
-          alt=""
-        />
-      </div>
-      <div className="scrollable-carousel__item">
-        <img
-          src="https://pics.filmaffinity.com/Bloodshot-229984487-large.jpg"
-          alt=""
-        />
-      </div>
-      <div className="scrollable-carousel__item">
-        <img
-          src="https://pics.filmaffinity.com/Bloodshot-229984487-large.jpg"
-          alt=""
-        />
+        {posters && posters.map(poster => {
+          return <div className="scrollable-carousel__item">
+            <img 
+              src={`https://image.tmdb.org/t/p/w185/${poster.image}`}></img>
+          </div>
+        })}
       </div>
     </div>
-  </div>
-   )    
+  )
 }
--- staff/groups/the-movie-records/movie-app/js/pages/home.jsx
+++ staff/groups/the-movie-records/movie-app/js/pages/home.jsx
@@ -7,10 +7,15 @@ constructor(){
 }
 
 componentWillMount() {
-    retrieveNewMovies(2,"es", (error,movies) => {
+    retrieveNewMovies(1,"es", (error,newMovies) => {
         if (error) return alert(error.message)
         
-        this.setState({ movies })
+        this.setState({ newMovies })
+    })
+    retrieveUpcomingMovies(1,"es", (error,upcomingMovies) => {
+        if (error) return alert(error.message)
+        
+        this.setState({ upcomingMovies })
     })
 }
 /* handleNewMovies = () => {retrieveNewMovies (function(pages, language, callback))} */
@@ -19,8 +24,8 @@ render(){
         <Header />
         <Discover />
         <Search />
-         {this.state.movies && <Slider title = "Cartelera de cine"  items = {this.state.movies}  />} 
-        <Slider title = "Novedades" />
+         {this.state.newMovies && <Slider title = "Cartelera de cine"  items = {this.state.newMovies}  />} 
+         {this.state.upcomingMovies && <Slider title = "Novedades"  items = {this.state.upcomingMovies}  />} 
         <Footer /> 
         </>
 
