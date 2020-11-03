fa8a0471c779e8c1fc9d566ccd47a6df72f8aa5d
Cristian Manzano Rodriguez <CRIS@MacBook-Pro-de-Cristian.local>
2020-11-03 19:48:41 +0100

    adding styles

--- staff/groups-project/los-spotys/the-bohemians/components/title.jsx
+++ staff/groups-project/los-spotys/the-bohemians/components/title.jsx
@@ -1,7 +1,5 @@
 function Title({ onHome }) {
     return (
-    <header>
         <h1 className="title" onClick={onHome}> The Bohemians </h1>
-    </header>
     );
 }
--- staff/groups-project/los-spotys/the-bohemians/components/welcome.jsx
+++ staff/groups-project/los-spotys/the-bohemians/components/welcome.jsx
@@ -1,6 +1,6 @@
 function Welcome({name, image}) {
     return <section className="welcome">
-    <img className= "welcome__image" src={image || "https://previews.123rf.com/images/ljupco/ljupco1711/ljupco171100141/89400762-disparo-de-perfil-de-longitud-completa-de-una-chica-punk-cantando-y-tocando-una-guitarra-el%C3%A9ctrica-ais.jpg"} />
+    <img className= "welcome__image" src={image || "https://i.pinimg.com/originals/50/05/f5/5005f514424141acf70727360add163d.png"} />
     <h2 className="welcome__title">Welcome {name}, to The Bohemians!</h2>
 </section>
 }
