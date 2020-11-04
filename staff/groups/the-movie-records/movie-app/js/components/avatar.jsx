f44a756e91efb49bd29ff2f2e6637584443d1e8e
Siscu Cano <siscuca@gmail.com>
2020-11-04 19:08:05 +0100

    Fix avatar image

--- staff/groups/the-movie-records/movie-app/js/components/avatar.jsx
+++ staff/groups/the-movie-records/movie-app/js/components/avatar.jsx
@@ -1,6 +1,10 @@
 function Avatar(props) {
-  const { token, onAvatar, onProfile } = props;
-  const user = JSON.parse(localStorage.getItem("user"));
+  const { token, onAvatar, onProfile, user } = props;
+  try {
+    const user = JSON.parse(localStorage.getItem("user"));
+  } catch (e) {
+    const user = null;
+  }
   return (
     <div
       className="avatar"
@@ -9,7 +13,11 @@ function Avatar(props) {
       <div className="avatar__initials">SG</div>
       <img
         className="avatar__image"
-        src= {token ? user.image : "https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png" }
+        src={
+          user && token
+            ? user.image
+            : "https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png"
+        }
         alt="alt"
       />
       <div className="avatar__notifications">01</div>
