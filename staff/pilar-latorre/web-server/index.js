d6b8cc02394405bd970f68ea477d52672aad911c
PILARLATORREUSAN <pilarlatorre82@gmail.com>
2020-11-09 19:30:50 +0100

    last update web-server 9/11

--- staff/pilar-latorre/web-server/index.js
+++ staff/pilar-latorre/web-server/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fs = require('fs')
+
 const app = express ()
 const port = 3000
 
@@ -9,10 +11,97 @@ app.get('/',(req, res) => {
 
 app.get('/helloworld', (req, res) => {
 
-    fs.readFile('./public')
+    fs.readFile('./public/helloworld/index.html', 'utf8', (error, content) => {
+        if(error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
 
+        res.send(content)
+    })
 })
 
+app.get('/register', (req, res) => {
+
+    fs.readFile('./public/register/index.html', 'utf8', (error, content) => {
+        if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
+
+        res.send(content)
+    })
+})
+
+app.post('/register', (req, res) => {
+    req.setEncoding('utf8')
+
+    let content = ''
+
+    req.on('data', chunk => {
+
+        content += chunk
+
+    })
+
+    req.on('end', () => {
+
+        const parts = content.split('&')
+
+        let [, fullname] = parts[0].split('=')
+        let [, email] = parts[1].split('=')
+        let [, password] = parts[2].split('=')
+
+        fullname = fullname.split('+').join(' ')
+
+        email = decodeURIComponent(email)
+
+        password = decodeURIComponent(password)
+
+        const id = `ID-${Math.random()}-${Date.now()}`
+
+        const user = { id, fullname, email, password}
+
+        const json = JSON.stringify(user)
+
+        fs.writeFile(`./data/users/${id}.json`, json, error => {
+            if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
+
+            res.send(`ok, user registered ,) ID: ${id}`)
+        })
+    })
+})
+
+
+app.get('/login', (req, res) => {
+    fs.readFile('./public/login/index.html', 'utf8', (error, content) => {
+        if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
+
+        res.send(content)
+    })
+})
+
+
+app.get('/login', (req, res) =>{
+    req.setEncoding('utf8')
+    
+    let content = ''
+
+    req.on('data', chunk => {
+
+        content += chunk
+    })
+
+    req.on('end', () => {
+        const parts = content.split('&')
+
+        let [, email] = parts[1].split('=')
+        let [, password] = parts[2].split('=')
+
+        email = decodeURIComponent(email)
+
+        password = decodeURIComponent(password)
+
+        console.log(email, password)
+
+        res.send('ok, logged in')
+    })
+
+})
 
 
 app.listen(port,()=>{
