de58ea9fa6a13fb2130a69976c736f4c7d72d2dd
PILARLATORREUSAN <pilarlatorre82@gmail.com>
2020-11-09 18:08:18 +0100

    begining server

---
+++ staff/pilar-latorre/web-server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express ()
+const port = 3000
+
+app.get('/',(req, res) => {
+    res.send ('Hello World!')
+
+})
+
+app.get('/helloworld', (req, res) => {
+
+    fs.readFile('./public')
+
+})
+
+
+
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+})
