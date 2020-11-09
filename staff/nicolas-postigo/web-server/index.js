d943ca5b4e6cf5ceea46e30e78fa401d7c717da5
Nicolas <nicolaspostigoausere@hotmail.es>
2020-11-09 20:40:41 +0100

    updates

--- staff/nicolas-postigo/web-server/index.js
+++ staff/nicolas-postigo/web-server/index.js
@@ -10,17 +10,37 @@ app.get('/helloworld', (req, res) => {
 
     //res.send('Hello World!')
 
-/*     res.set('Content-type', 'text/plain')
-    res.set('Access-Control-Allow-Origin', '*')
+    /*     res.set('Content-type', 'text/plain')
+        res.set('Access-Control-Allow-Origin', '*')
+    
+        res.send(`hola mundo`) */
 
-    res.send(`hola mundo`) */
+    fs.readFile('./public/index.html', 'utf8', (error, content) => {
+        if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
 
-    fs.readFile('./public/helloworld/index.html', 'utf8', (error, content) => {
-         if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
-
-         res.send(content)
+        res.send(content)
     })
 })
+
+app.get('/login', (req, res) => {
+
+
+
+    fs.readFile('./public/login/index.html', 'utf8', (error, content) => {
+        if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
+
+        res.send(content)
+    })
+})
+
+app.post('/login', (req, res) => {
+    res.send("ok ;)")
+
+
+    })
+
+
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
