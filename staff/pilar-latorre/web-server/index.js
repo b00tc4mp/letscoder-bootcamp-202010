645a821588f8413f981d7b3589b3506758a974d4
PILARLATORREUSAN <pilarlatorre82@gmail.com>
2020-11-11 19:39:15 +0100

    update index & home

--- staff/pilar-latorre/web-server/index.js
+++ staff/pilar-latorre/web-server/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const fs = require('fs')
-
-const app = express ()
+const app = express()
 const port = 3000
+const fs = require('fs')
+const path = require('path')
+const authenticateUser = require('./logic/authenticate-user')
+const retrieveUser = require('./logic/retrieve-user')
+
 
 
 app.get('/register', (req, res) => {
 
-    fs.readFile('./public/register/index.html', 'utf8', (error, content) => {
+    fs.readFile(path.join(__dirname,'./public/register/index.html'), 'utf8', (error, content) => {
         if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
 
         res.send(content)
@@ -40,13 +43,13 @@ app.post('/register', (req, res) => {
 
         password = decodeURIComponent(password)
 
-        const id = `${Date.now()}${`${Math.random() * 10**18}`.padStart(18, '0')}`
+        const id = `${Date.now()}${`${Math.random() * 10 ** 18}`.padStart(18, '0')}`
 
         const user = { id, fullname, email, password}
 
         const json = JSON.stringify(user)
 
-        fs.writeFile(`./data/users/${id}.json`, json, error => {
+        fs.writeFile(path.join(__dirname,`./data/users/${id}.json`), json, error => {
             if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
 
             res.send(`ok, user registered ,) ID: ${id}`)
@@ -56,15 +59,16 @@ app.post('/register', (req, res) => {
 
 
 app.get('/login', (req, res) => {
-    fs.readFile('./public/login/index.html', 'utf8', (error, content) => {
+    fs.readFile(path.join(__dirname,'./public/login/index.html'), 'utf8', (error, content) => {
         if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
 
         res.send(content)
     })
 })
 
+let session 
 
-app.get('/login', (req, res) =>{
+app.post('/login', (req, res) =>{
     req.setEncoding('utf8')
     
     let content = ''
@@ -77,18 +81,51 @@ app.get('/login', (req, res) =>{
     req.on('end', () => {
         const parts = content.split('&')
 
-        let [, email] = parts[1].split('=')
-        let [, password] = parts[2].split('=')
+        let [, email] = parts[0].split('=')
+        let [, password] = parts[1].split('=')
 
         email = decodeURIComponent(email)
 
         password = decodeURIComponent(password)
 
-        console.log(email, password)
+        authenticateUser(email, password, (error, userId) => {
+            if(error) {
+                return fs.readFile(path.join(__dirname, './public/error/index.html'), 'utf8', (error, content) => {
+                    if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
 
-        res.send('ok, logged in')
+                    res.send(content)
+                })
+            }
+            session = userId
+
+            res.redirect('/')
+
+        })
+
+
     })
 
+})
+
+app.get('/', (req, res) => {
+    if (session)
+        fs.readFile(path.join(__dirname, './public/home/index.html'), 'utf8', (error, content) => {
+            if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
+
+            retrieveUser(session, (error, user) => {
+                if (error) {
+                    return fs.readFile(path.join(__dirname, './public/error/index.html'), 'utf8', (error, content) => {
+                        if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
+
+                        res.send(content)
+                    })
+                }
+
+                res.send(content.replace('{fullname}', user.fullname))
+            })
+
+        })
+    else res.redirect('/login')
 })
 
 
