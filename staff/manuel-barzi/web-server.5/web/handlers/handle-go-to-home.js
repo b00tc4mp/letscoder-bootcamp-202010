1f62d5665555ecf20f96f533aa358f01b0b92703
manuelbarzi <manuelbarzi@gmail.com>
2020-11-13 18:53:18 +0100

    boost web-server

--- staff/manuel-barzi/web-server.5/web/handlers/handle-go-to-home.js
+++ staff/manuel-barzi/web-server.5/web/handlers/handle-go-to-home.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
-const retrieveUser = require('../logic/retrieve-user')
-const sessions = require('../sessions')
+const retrieveUser = require('../../logic/retrieve-user')
+const sessions = require('../../sessions')
 
 module.exports = (req, res) => {
     const { cookies: { 'session-id': sessionId } }  = req
@@ -11,13 +11,13 @@ module.exports = (req, res) => {
     if (session && session.userId)
         retrieveUser(session.userId, (error, user) => {
             if (error)
-                return fs.readFile(path.join(__dirname, '../views/error.html'), 'utf8', (_error, content) => {
+                return fs.readFile(path.join(__dirname, '../../views/error.html'), 'utf8', (_error, content) => {
                     if (_error) return res.send(`sorry, there was an error :( ERROR: ${_error.message}`)
 
                     res.send(content.replace('{message}', error.message))
                 })
 
-            fs.readFile(path.join(__dirname, '../views/home.html'), 'utf8', (error, content) => {
+            fs.readFile(path.join(__dirname, '../../views/home.html'), 'utf8', (error, content) => {
                 if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
 
                 res.send(content.replace('{fullname}', user.fullname))
