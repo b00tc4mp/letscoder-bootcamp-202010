0a78113b3b6a5796d299c0001e626aa14a27e351
Aida Montero <aidacastro1996@gmail.com>
2020-11-14 22:45:37 +0100

    add changes

--- staff/aiida-montero/web-server/handlers/handle-go-to-register.js
+++ staff/aiida-montero/web-server/handlers/handle-go-to-register.js
@@ -1,14 +1,23 @@
 const fs = require('fs')
 const path = require ('path')
+const sessions = require('../sessions')
+const {createId} = require('../utils/ids')
+const {createSessionCookie} = require('./helpers/cookies')
 
 module.exports = (req, res) => {
-   const{ cookies : {'session-id' : userId }} = req
+   const{ cookies : {'session-id' : sessionId = createId() }} = req
+
+   res.setHeader('set-cookie', createSessionCookie(sessionId))
+
+   const session = sessions[sessionId] || (session[sessionId] = {} )
+
+   const {userId, cookiesAccepted } = session
 
    if (!userId)
       fs.readFile(path.join(__dirname, '../views/register.html'), 'utf8', (error, content) => {
        if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
 
-       res.send(content)
+       res.send(content.replace('{cookiesAccepted}', cookiesAccepted ))
 
       })
 
