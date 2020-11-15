da2b41134ef50ef5803901a566a2a83ebaad6698
PILARLATORREUSAN <pilarlatorre82@gmail.com>
2020-11-15 12:15:42 +0100

    last update web-server

--- staff/pilar-latorre/web-server/index.js
+++ staff/pilar-latorre/web-server/index.js
@@ -5,14 +5,23 @@ const port = 3000
 const urlencondedBodyParser = require('./middlewares/urlencoded-body-parser')
 const cookieParser = require('./middlewares/cookie-parser')
 
-const handleGoToRegister = require('./handlers/handle-go-to-register')
-const handleRegister = require('./handlers/handle-register')
-const handleGoToLogin = require('./handlers/handle-go-to-login')
-const handleLogin = require('./handlers/handle-login')
-const handleGoToHome = require('./handlers/handle-go-to-home')
-const handleLogout = require('./handlers/handle-logout')
-const handleNotFound = require('./handlers/handle-not-found')
+const {
+    handleGoToRegister,
+    handleRegister,
+    handleGoToLogin,
+    handleLogin,
+    handleGoToHome,
+    handleLogout,
+    handleNotFound,
+    handleGoToSearch
 
+} = require('./web/handlers')
+
+const {
+    handlAcceptCookies
+
+} = require('./api/handlers')
+const handleAcceptCookies = require('./api/handlers/handle-accept-cookies')
 
 app.use(express.static('public'))
 
@@ -22,11 +31,15 @@ app.post('/register', urlencondedBodyParser, handleRegister)
 
 app.get('/login', cookieParser, handleGoToLogin)
 
-app.post('/login', urlencondedBodyParser, handleLogin)
+app.post('/login', cookieParser, urlencondedBodyParser, handleLogin)
 
 app.get('/', cookieParser, handleGoToHome)
 
 app.post('/logout', handleLogout)
+
+app.get('/search', cookieParser, handleGoToSearch)
+
+app.post('/api/accept-cookies', cookieParser, handleAcceptCookies)
 
 app.get('/*', handleNotFound)
 
--- staff/pilar-latorre/web-server/web/handlers/handle-register.js
+++ staff/pilar-latorre/web-server/web/handlers/handle-register.js
@@ -7,13 +7,13 @@ module.exports = (req, res) => {
 
     registerUser(fullname, email, password, error => {
         if (error)
-            return fs.readFile(path.join(__dirname,'../views/error.html'), 'utf8', (_error, content) => {
+            return fs.readFile(path.join(__dirname,'../../views/error.html'), 'utf8', (_error, content) => {
                 if (_error) return res.send(`sorry, there was an error :( ERROR: ${_error.message}`)
         
                 res.send(content.replace('{message}', error.message))
             })
 
-            fs.readFile(path.join(__dirname,'../views/register-confirm.html'), 'utf8', (error, content) => {
+            fs.readFile(path.join(__dirname,'../../views/register-confirm.html'), 'utf8', (error, content) => {
                 if (error) return res.send(`sorry, there was an error :( ERROR: ${error.message}`)
 
                 res.send(content)
