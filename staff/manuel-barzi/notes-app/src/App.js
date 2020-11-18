55f70e69f7449183c31773f980fc81a88f90a1ae
manuelbarzi <manuelbarzi@gmail.com>
2020-11-18 15:06:32 +0100

    initial code

---
+++ staff/manuel-barzi/notes-app/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { SignUp, SignIn } from './components'
+import { useState } from 'react'
+import { registerUser } from './logic'
+
+function App() {
+  const [view, setView] = useState('sign-up')
+
+  const handleSignUp = (fullname, email, password) => {
+    registerUser(fullname, email, password, error => {
+      if (error) return alert(error.message)
+
+      setView('sign-in')
+    })
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, Manu!</h1>
+
+        {view === 'sign-up' && <SignUp onSignUp={handleSignUp} />}
+        {view === 'sign-in' && <SignIn />}
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ staff/manuel-barzi/notes-app/src/logic/index.js
@@ -0,0 +1,5 @@
+import registerUser from './register-user'
+
+export {
+    registerUser
+}
