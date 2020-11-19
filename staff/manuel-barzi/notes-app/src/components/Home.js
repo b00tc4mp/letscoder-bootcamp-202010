f3d1444d55450f95f30dccb2f3f85fc0eba07cc3
manuelbarzi <manuelbarzi@gmail.com>
2020-11-19 14:41:49 +0100

    boost notes-apx

--- staff/manuel-barzi/notes-app/src/components/Home.js
+++ staff/manuel-barzi/notes-app/src/components/Home.js
@@ -1,5 +1,27 @@
 import './Home.sass'
+import { useState, useEffect } from 'react'
+import { retrieveUser } from '../logic'
 
 export default function () {
-    return <h1>Hello, World!</h1>
+    const [name, setName] = useState()
+
+    useEffect(() => {
+        const { token } = sessionStorage
+
+        try {
+            retrieveUser(token, (error, user) => {
+                if (error) return alert(error.message)
+
+                const { fullname } = user
+
+                setName(fullname)
+            })
+        } catch (error) {
+            alert(error.message)
+        }
+    }, [])
+
+    return <section className="home">
+        <h1>Hello, {name}!</h1>
+    </section>
 }
--- staff/manuel-barzi/notes-app/src/logic/index.js
+++ staff/manuel-barzi/notes-app/src/logic/index.js
@@ -1,7 +1,9 @@
 import registerUser from './register-user'
 import authenticateUser from './authenticate-user'
+import retrieveUser from './retrieve-user'
 
 export {
     registerUser,
-    authenticateUser
+    authenticateUser,
+    retrieveUser
 }
