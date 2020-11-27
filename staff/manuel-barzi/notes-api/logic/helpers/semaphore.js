ccd6538658b88064259d257e641263d9731f1836
manuelbarzi <manuelbarzi@gmail.com>
2020-11-27 13:24:14 +0100

    correct semaphore to work properly with promises

--- staff/manuel-barzi/notes-api/logic/helpers/semaphore.js
+++ staff/manuel-barzi/notes-api/logic/helpers/semaphore.js
@@ -1,3 +1,3 @@
 let chain = Promise.resolve()
 
-module.exports = callback => chain = chain.then(callback)
+module.exports = callback => chain = chain.then(callback, callback)
--- staff/manuel-barzi/notes-api/logic/register-user.demo.js
+++ staff/manuel-barzi/notes-api/logic/register-user.demo.js
@@ -6,7 +6,13 @@ const registerUser = require('./register-user')
 const { env: { MONGODB_URL } } = process
 
 mongoose.connect(MONGODB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => registerUser('Pa Járito', 'pajarin@mail.com', '123123123').then(console.log).catch(console.error))
+    .then(() => Promise.all([
+        registerUser('Pa Járito', 'pajarin@mail.com', '123123123').then(console.log).catch(console.error),
+        registerUser('Pa Járito', 'pajarin@mail.com', '123123123').then(console.log).catch(console.error),
+        registerUser('Pa Járito', 'pajarin@mail.com', '123123123').then(console.log).catch(console.error),
+        registerUser('Gu Sánito', 'gusanin@mail.com', '123123123').then(console.log).catch(console.error)
+    ])
+    )
     .catch(console.error)
     .then(() => mongoose.disconnect())
     .then(() => console.log('ended'))
