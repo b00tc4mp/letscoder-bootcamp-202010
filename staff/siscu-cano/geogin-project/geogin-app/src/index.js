a03a4525bca463597d386b2bbd662da0409b2840
Siscu Cano <siscuca@gmail.com>
2020-12-01 16:29:38 +0100

    Add standard linter

--- staff/siscu-cano/geogin-project/geogin-app/src/index.js
+++ staff/siscu-cano/geogin-project/geogin-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Test webApp!</h1>, document.getElementById('app'))
+ReactDOM.render(<h1>Test webApp! aaaa</h1>, document.getElementById('app'))
--- staff/siscu-cano/geogin-project/geogin-app/webpack.config.js
+++ staff/siscu-cano/geogin-project/geogin-app/webpack.config.js
@@ -1,37 +1,37 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-        output: {
-                filename: 'app.bundle.js'
-        },
-        devServer: {
-            contentBase: path.join(__dirname, 'dist'),
-            compress: true,
-            port: 9000,
-            publicPath: path.join(__dirname, 'dist'),
-            open: true
-        },
-        plugins: [
-                new HtmlWebpackPlugin({
-                        template: 'src/index.html'
-                })
-        ],
-        module: {
-                rules: [
-                    {
-                        test: /\.js$/,
-                        exclude: /node_modules/,
-                        use: {
-                            loader: 'babel-loader',
-                            options: {
-                                presets: [
-                                    '@babel/preset-env', 
-                                    '@babel/preset-react'
-                                ]
-                            }
-                        }
-                    }
-                ]
-            }
+  output: {
+    filename: 'app.bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    publicPath: path.join(__dirname, 'dist'),
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
 }
