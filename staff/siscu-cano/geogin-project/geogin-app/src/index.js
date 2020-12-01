8fdc0594cea0a1bc9da016432f43b62925bb464f
Siscu Cano <siscuca@gmail.com>
2020-12-01 15:59:12 +0100

    Add React and Babel

--- staff/siscu-cano/geogin-project/geogin-app/src/index.js
+++ staff/siscu-cano/geogin-project/geogin-app/src/index.js
@@ -1,1 +1,4 @@
-console.log("Test");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Test webApp!</h1>, document.getElementById('app'))
--- staff/siscu-cano/geogin-project/geogin-app/webpack.config.js
+++ staff/siscu-cano/geogin-project/geogin-app/webpack.config.js
@@ -13,6 +13,25 @@ module.exports = {
             open: true
         },
         plugins: [
-                new HtmlWebpackPlugin()
-        ]
+                new HtmlWebpackPlugin({
+                        template: 'src/index.html'
+                })
+        ],
+        module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [
+                                    '@babel/preset-env', 
+                                    '@babel/preset-react'
+                                ]
+                            }
+                        }
+                    }
+                ]
+            }
 }
