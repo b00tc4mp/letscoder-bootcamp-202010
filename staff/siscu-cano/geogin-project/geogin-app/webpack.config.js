fb6b94510749ee1183923433e9cf7dd9cbc7beaf
Siscu Cano <siscuca@gmail.com>
2020-12-01 12:26:35 +0100

    Add webpack devserver

--- staff/siscu-cano/geogin-project/geogin-app/webpack.config.js
+++ staff/siscu-cano/geogin-project/geogin-app/webpack.config.js
@@ -1,8 +1,16 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require('path');
 
 module.exports = {
         output: {
                 filename: 'app.bundle.js'
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 9000,
+            publicPath: path.join(__dirname, 'dist'),
+            open: true
         },
         plugins: [
                 new HtmlWebpackPlugin()
