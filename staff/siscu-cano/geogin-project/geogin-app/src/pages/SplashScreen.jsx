81074aee5e20ad98c78ce32bea8f73b6a8d45e66
Siscu Cano <siscuca@gmail.com>
2020-12-03 17:39:15 +0100

    Redirect from splashscreen to onboarding page

--- staff/siscu-cano/geogin-project/geogin-app/src/pages/SplashScreen.jsx
+++ staff/siscu-cano/geogin-project/geogin-app/src/pages/SplashScreen.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Redirect } from '@reach/router'
+
 import { useBodyClass } from '../hooks/useBodyClass'
 
 import { Logo } from '../components/Logo'
@@ -6,12 +8,25 @@ import { ProgressBar } from '../components/ProgressBar'
 import { Qrcode } from '../components/Qrcode'
 
 export const SplashScreen = () => {
+  const [loading, setLoading] = useState(true)
+  const timeToLoad = 3000
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), timeToLoad)
+  }, [])
+
   useBodyClass('splashscreen')
   return (
-    <div>
-      <Logo />
-      <Qrcode />
-      <ProgressBar done={50} />
-    </div>
+    <>
+      {loading === true ? (
+        <div>
+          <Logo />
+          <Qrcode />
+          <ProgressBar duration={timeToLoad} />
+        </div>
+      ) : (
+        <Redirect noThrow to='/onboarding' />
+      )}
+    </>
   )
 }
