2d12c277ac8bc8218c0f224690e608680dd9d793
Siscu Cano <siscuca@gmail.com>
2020-12-03 22:13:39 +0100

    Add react-slick slider in Onboarding page

--- staff/siscu-cano/geogin-project/geogin-app/src/components/Carousel/styles.js
+++ staff/siscu-cano/geogin-project/geogin-app/src/components/Carousel/styles.js
@@ -1,119 +1,345 @@
 import styled from 'styled-components'
 
-export const CarouselWrapper = styled.div`
+export const SliderWrapper = styled.div`
+  // BEGIN STYLES Slider
+  .slick-slider {
+    position: relative;
 
-  & {
-    --page-number: 1;
-    zoom: 0.9;
-    max-height: 100%;
-    width: 360px;
-    border-radius: 10px;
-    overflow: hidden;
-    margin: 0 auto;
+    display: block;
+    box-sizing: border-box;
 
-    & .slider {
-      width: 300%;
-      height: 100%;
-      display: flex;
-      background-color: #fff;
-      margin-left: calc(-360px * (var(--page-number) - 1));
-      transition: margin-left 400ms;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
 
-      .slide {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        max-height: 462px;
-
-        h1 {
-          padding: 0 20px;
-          margin: 24px 0 0 0;
-          font-family: 'Patrick Hand', cursive;
-          font-size: 30px;
-          color: #00bcc2;
-          text-align: center;
-        }
-
-        .img {
-          max-width: 300px;
-          margin: 0 auto;
-          height: auto;
-        }
-
-        p {
-          margin: 20px;
-          max-width: 250px;
-          font-size: 16px;
-          font-size: 19px;
-          line-height: 1.5em;
-          text-align: center;
-          color: #9e9e9e;
-        }
-      }
-    }
-
-    & .controls {
-      display: flex;
-      flex-direction: column;
-      flex-wrap: wrap;
-      background-color: #fff;
-      form {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex: 1;
-        padding: 20px;
-        input[type='radio'] {
-          outline: none;
-          margin: 0 5px;
-          opacity: 0;
-        }
-
-        input[type='radio'] + label {
-          position: relative;
-          top: -1px;
-          left: -17px;
-          height: 10px;
-          width: 10px;
-          border-radius: 100%;
-          border: 1px solid #ced7e6;
-          transition: border 300ms, background 300ms;
-        }
-
-        input[type='radio']:checked + label {
-          border: 1px solid #a5e24d;
-          background-color: #a5e24d;
-        }
-      }
-
-      a.btn-next {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 60px;
-        height: 65px;
-        background: #a5e24d;
-        color: white;
-        font-family: sans-serif;
-        font-size: 23px;
-        font-weight: bold;
-        text-decoration: none;
-      }
-    }
+    -webkit-touch-callout: none;
+    -khtml-user-select: none;
+    -ms-touch-action: pan-y;
+    touch-action: pan-y;
+    -webkit-tap-highlight-color: transparent;
   }
 
-  .thumbnail {
-    position: fixed;
+  .slick-list {
+    position: relative;
+
+    display: block;
+    overflow: hidden;
+
+    margin: 0;
+    padding: 0;
+  }
+  .slick-list:focus {
+    outline: none;
+  }
+  .slick-list.dragging {
+    cursor: pointer;
+    cursor: hand;
+  }
+
+  .slick-slider .slick-track,
+  .slick-slider .slick-list {
+    -webkit-transform: translate3d(0, 0, 0);
+    -moz-transform: translate3d(0, 0, 0);
+    -ms-transform: translate3d(0, 0, 0);
+    -o-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+
+  .slick-track {
+    position: relative;
     top: 0;
-    right: 0;
-    bottom: 0;
     left: 0;
-    background: url('https://s9.postimg.org/mv4hbyhhr/thumbnail.png');
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center center;
+
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .slick-track:before,
+  .slick-track:after {
+    display: table;
+
+    content: '';
+  }
+  .slick-track:after {
+    clear: both;
+  }
+  .slick-loading .slick-track {
+    visibility: hidden;
+  }
+
+  .slick-slide {
     display: none;
+    float: left;
+
+    height: 100%;
+    min-height: 1px;
+  }
+  [dir='rtl'] .slick-slide {
+    float: right;
+  }
+  .slick-slide img {
+    display: block;
+  }
+  .slick-slide.slick-loading img {
+    display: none;
+  }
+  .slick-slide.dragging img {
+    pointer-events: none;
+  }
+  .slick-initialized .slick-slide {
+    display: block;
+  }
+  .slick-loading .slick-slide {
+    visibility: hidden;
+  }
+  .slick-vertical .slick-slide {
+    display: block;
+
+    height: auto;
+
+    border: 1px solid transparent;
+  }
+  .slick-arrow.slick-hidden {
+    display: none;
+  }
+
+  // theme 
+
+  /* Slider */
+.slick-loading .slick-list
+{
+    background: #fff url('./ajax-loader.gif') center center no-repeat;
+}
+
+/* Icons */
+@font-face
+{
+    font-family: 'slick';
+    font-weight: normal;
+    font-style: normal;
+
+    src: url('./fonts/slick.eot');
+    src: url('./fonts/slick.eot?#iefix') format('embedded-opentype'), url('./fonts/slick.woff') format('woff'), url('./fonts/slick.ttf') format('truetype'), url('./fonts/slick.svg#slick') format('svg');
+}
+/* Arrows */
+.slick-prev,
+.slick-next
+{
+    font-size: 0;
+    line-height: 0;
+
+    position: absolute;
+    top: 50%;
+
+    display: block;
+
+    width: 20px;
+    height: 20px;
+    padding: 0;
+    -webkit-transform: translate(0, -50%);
+    -ms-transform: translate(0, -50%);
+    transform: translate(0, -50%);
+
+    cursor: pointer;
+
+    color: transparent;
+    border: none;
+    outline: none;
+    background: transparent;
+}
+.slick-prev:hover,
+.slick-prev:focus,
+.slick-next:hover,
+.slick-next:focus
+{
+    color: transparent;
+    outline: none;
+    background: transparent;
+}
+.slick-prev:hover:before,
+.slick-prev:focus:before,
+.slick-next:hover:before,
+.slick-next:focus:before
+{
+    opacity: 1;
+}
+.slick-prev.slick-disabled:before,
+.slick-next.slick-disabled:before
+{
+    opacity: .25;
+}
+
+.slick-prev:before,
+.slick-next:before
+{
+    font-family: 'slick';
+    font-size: 20px;
+    line-height: 1;
+
+    opacity: .75;
+    color: white;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+.slick-prev
+{
+    left: -25px;
+}
+[dir='rtl'] .slick-prev
+{
+    right: -25px;
+    left: auto;
+}
+.slick-prev:before
+{
+    content: '←';
+}
+[dir='rtl'] .slick-prev:before
+{
+    content: '→';
+}
+
+.slick-next
+{
+    right: -25px;
+}
+[dir='rtl'] .slick-next
+{
+    right: auto;
+    left: -25px;
+}
+.slick-next:before
+{
+    content: '→';
+}
+[dir='rtl'] .slick-next:before
+{
+    content: '←';
+}
+
+/* Dots */
+.slick-dotted.slick-slider
+{
+    margin-bottom: 30px;
+}
+
+.slick-dots
+{
+    position: absolute;
+    bottom: -25px;
+
+    display: block;
+
+    width: 100%;
+    padding: 0;
+    margin: 0;
+
+    list-style: none;
+
+    text-align: center;
+}
+.slick-dots li
+{
+    position: relative;
+
+    display: inline-block;
+
+    width: 20px;
+    height: 20px;
+    margin: 0 5px;
+    padding: 0;
+
+    cursor: pointer;
+}
+.slick-dots li button
+{
+    font-size: 0;
+    line-height: 0;
+
+    display: block;
+
+    width: 20px;
+    height: 20px;
+    padding: 5px;
+
+    cursor: pointer;
+
+    color: transparent;
+    border: 0;
+    outline: none;
+    background: transparent;
+}
+.slick-dots li button:hover,
+.slick-dots li button:focus
+{
+    outline: none;
+}
+.slick-dots li button:hover:before,
+.slick-dots li button:focus:before
+{
+    opacity: 1;
+}
+.slick-dots li button:before
+{
+    font-family: 'slick';
+    font-size: 6px;
+    line-height: 20px;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 20px;
+    height: 20px;
+
+    content: '•';
+    text-align: center;
+
+    opacity: .25;
+    color: black;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+.slick-dots li.slick-active button:before
+{
+    opacity: .75;
+    color: black;
+}
+
+// END STYLES Slider 
+  background: #fff;
+  border-radius: 16px;
+
+  .slick-dots li button:before {
+    font-size: 12px;
+    color: #a3ff1e;
+  }
+
+  h1 {
+    padding: 0 20px;
+    margin: 24px 0 0 0;
+    font-family: 'Patrick Hand', cursive;
+    font-size: 30px;
+    color: #b3b4b5;
+    text-align: center;
+  }
+
+  .img {
+    max-width: 300px;
+    margin: 0 auto;
+    height: auto;
+  }
+
+  p {
+    margin: 20px;
+    font-size: 16px;
+    font-size: 19px;
+    line-height: 1.5em;
+    text-align: center;
+    color: #9e9e9e;
   }
 `
