d4da8a5cdc232c1deedfa5b4e8ee47d4d2f5668f
Siscu Cano <siscuca@gmail.com>
2020-12-07 23:24:20 +0100

    Add UploadImage in page create search

--- staff/siscu-cano/geogin-project/geogin-app/src/pages/SearchCreate.jsx
+++ staff/siscu-cano/geogin-project/geogin-app/src/pages/SearchCreate.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useBodyClass } from '../hooks/useBodyClass'
+import { UploadImage } from '../components/UploadImage'
 
 export const SearchCreate = () => {
   useBodyClass('searchcreate')
   return (
     <>
       <h1>Componente: SearchCreate</h1>
+      <UploadImage />
     </>
   )
 }
