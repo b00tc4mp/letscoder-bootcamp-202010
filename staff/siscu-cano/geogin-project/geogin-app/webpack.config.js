34c33a636df921c0fd4b3811494c33546b5a96f8
Siscu Cano <siscuca@gmail.com>
2020-12-07 23:23:28 +0100

    Add css loader in webpack

--- staff/siscu-cano/geogin-project/geogin-app/webpack.config.js
+++ staff/siscu-cano/geogin-project/geogin-app/webpack.config.js
@@ -43,6 +43,10 @@ module.exports = {
             limit: 90000
           }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
