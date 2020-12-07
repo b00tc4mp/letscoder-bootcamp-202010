4dfbc01136018adba0193b6cd71d1a2f298b7b68
Nicolas <nicolaspostigoausere@hotmail.es>
2020-12-07 13:33:49 +0100

    next step: detail

--- staff/nicolas-postigo/huertea-api/logic/index.js
+++ staff/nicolas-postigo/huertea-api/logic/index.js
@@ -1,10 +1,10 @@
-const retrieveOffer = require('./retrieve-offer');
+
 
 module.exports = {
     registerUser: require('./register-user'),
     authenticateUser: require('./authenticate-user'),
     retrieveUser: require('./retrieve-user'),
     createOffer: require('./create-offer.js'),
-    retrieveOffer: require('./retrieve-offer.js'),
+    retrieveOffer: require('./retrieve-all-offers.js'),
     findOffer: require('./find-offer.js')
 }
