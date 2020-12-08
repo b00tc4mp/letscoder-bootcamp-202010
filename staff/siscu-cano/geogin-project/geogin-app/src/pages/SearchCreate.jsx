617d23744dd324582b404dbb55ef05ecad778f49
Siscu Cano <siscuca@gmail.com>
2020-12-08 14:34:50 +0100

    Add step component in create search page

--- staff/siscu-cano/geogin-project/geogin-app/src/pages/SearchCreate.jsx
+++ staff/siscu-cano/geogin-project/geogin-app/src/pages/SearchCreate.jsx
@@ -1,13 +1,72 @@
 import React from 'react'
 import { useBodyClass } from '../hooks/useBodyClass'
 import { UploadImage } from '../components/UploadImage'
+import StepProgressBar from 'react-step-progress'
+import 'react-step-progress/dist/index.css'
+import { DefaultLayout } from './searchCreateStyles'
 
 export const SearchCreate = () => {
   useBodyClass('searchcreate')
-  return (
+
+  // setup the step content
+  const step1Content = (
     <>
-      <h1>Componente: SearchCreate</h1>
+      <p className='description'>1.) Inserta la imagen que deseas que aparezca como portada de la búqueda.</p>
       <UploadImage />
     </>
+  )
+
+  const step2Content = <h1>Step 2 Content</h1>
+  const step3Content = <h1>Step 3 Content</h1>
+
+  // setup step validators, will be called before proceeding to the next step
+  function step2Validator () {
+  // return a boolean
+  }
+
+  function step3Validator () {
+  // return a boolean
+  }
+
+  function onFormSubmit () {
+  // handle the submit logic here
+  // This function will be executed at the last step
+  // when the submit button (next button in the previous steps) is pressed
+  }
+
+  return (
+    <DefaultLayout>
+      <h1>Crear nueva búsqueda</h1>
+      <StepProgressBar
+        startingStep={0}
+        onSubmit={onFormSubmit}
+        completeColor='#4db193'
+        defaultColor='#4db193'
+        activeColor='#4db193'
+        circleFontColor='#4db193'
+        steps={[
+          {
+            label: 'Búsqueda',
+            subtitle: '',
+            name: 'step 1',
+            content: step1Content
+          },
+          {
+            label: 'Pruebas',
+            subtitle: '',
+            name: 'step 2',
+            content: step2Content,
+            validator: step2Validator
+          },
+          {
+            label: 'Participantes',
+            subtitle: '',
+            name: 'step 3',
+            content: step3Content,
+            validator: step3Validator
+          }
+        ]}
+      />
+    </DefaultLayout>
   )
 }
---
+++ staff/siscu-cano/geogin-project/geogin-app/src/pages/searchCreateStyles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const DefaultLayout = styled.div`
+   background: #f3f7ff;
+    width: 90%;
+    margin: 0 auto;
+    border-radius: 8px;
+    border: 4px solid #b1d8ff85;
+    height: calc(100% - 50px);
+    max-height: 100%;
+    h1 {
+        color: #1689fe;
+        font-size: 28px;
+        margin: 0 auto;
+        text-align: center;
+        margin: 20px 0;
+        text-shadow: 2px 2px #d0e1f5;
+        font-family: 'Patrick Hand',cursive;
+    }
+    ul {
+        margin-bottom: 40px;
+    }
+    .description {
+        font-size: 14px;
+    }
+   
+`
