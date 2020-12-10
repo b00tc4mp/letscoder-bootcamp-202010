c2282b4ca997aac111d6c90e493a0067379f8404
Siscu Cano <siscuca@gmail.com>
2020-12-10 19:44:29 +0100

    Add Textarea component

---
+++ staff/siscu-cano/geogin-project/geogin-app/src/components/TextArea/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const TextAreaWrapper = styled.div`
+    textarea {
+        width: 100%;
+        height: 60px;
+        padding: 8px 4px 8px 10px;
+        margin-bottom: 15px;
+        border: 1px solid #4e3043;
+        border: 1px solid rgba(78,48,67,0.8);
+        background: rgb(255 255 255);
+        border-radius: 2px;
+        box-shadow: 0 1px 0 rgba(255,255,255,0.2), inset 0 1px 1px rgba(0,0,0,0.1);
+        -webkit-transition: all 0.3s ease-out;
+        transition: all 0.3s ease-out;
+        font-family: 'Raleway','Lato',Arial,sans-serif;
+        color: rgb(13 107 202);
+        font-size: 13px;
+    }
+`
