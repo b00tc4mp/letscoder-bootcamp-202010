a334b958d3b30b0ff618f3b3925d13468c00af37
Siscu Cano <siscuca@gmail.com>
2020-12-12 00:37:35 +0100

    Linter error

--- staff/siscu-cano/geogin-project/geogin-data/models/index.js
+++ staff/siscu-cano/geogin-project/geogin-data/models/index.js
@@ -1,10 +1,10 @@
 const { model } = require('mongoose')
-const { user, note } = require('./schemas')
+const { user, game, quest, point, evaluation } = require('./schemas')
 
-const User = model('User', user),
-const Game = model('Game', game),
-const Quest = model('Quest', quest),
-const Point = model('Point', point),
+const User = model('User', user)
+const Game = model('Game', game)
+const Quest = model('Quest', quest)
+const Point = model('Point', point)
 const Evaluation = model('Evaluation', evaluation)
 
 User.createIndexes()
