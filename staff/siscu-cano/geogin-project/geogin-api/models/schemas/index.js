48f8b334670cb8e6e207dd53da94e246edce1f9d
Siscu Cano <siscuca@gmail.com>
2020-12-11 21:55:46 +0100

    Remove models mongoose in API server

--- staff/siscu-cano/geogin-project/geogin-api/models/schemas/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    user: require('./user'),
-    game: require('./game'),
-    point: require('./point'),
-    quest: require('./quest'),
-    evaluation: require('./evaluation')
-}
