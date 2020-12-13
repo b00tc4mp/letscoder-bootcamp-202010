1c85ae1f000a771f241c6bb9696752e69fee74ac
Siscu Cano <siscuca@gmail.com>
2020-12-13 19:14:46 +0100

    Qrcodes demo

--- staff/siscu-cano/geogin-project/geogin-app/src/pages/FavoritesList.jsx
+++ staff/siscu-cano/geogin-project/geogin-app/src/pages/FavoritesList.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useBodyClass } from '../hooks/useBodyClass'
 
+const QRCode = require('qrcode.react')
+
 export const FavoritesList = () => {
   useBodyClass('favoriteslist')
   return (
     <>
       <h1>Componente: FavoritesList</h1>
+      <QRCode renderAs='svg' value='http://facebook.github.io/react/' />
+
     </>
   )
 }
