9cf4a2698055108371b6dd31661e67a8e20169dd
PILARLATORREUSAN <pilarlatorre82@gmail.com>
2020-12-13 11:39:28 +0100

    update detail for shelter

--- staff/pilar-latorre/adogtapp-project/adogtapp-app/src/components/DetailPet.js
+++ staff/pilar-latorre/adogtapp-project/adogtapp-app/src/components/DetailPet.js
@@ -29,11 +29,11 @@ function DetailPet({result: {id, name, breed, species, color, description, userN
             <p className="result__p">species: {species}</p>
             <p className="result__p">color: {color}</p>
             <p className="result__p">description: {description}</p>
-
-            <p className="result__p">userName: {userName}</p>
-            <p className="result__p">email: {email}</p>
-            <p className="result__p">phone: {phone}</p>
-            <p className="result__p">description: {descriptionShelter}</p>
+            {!token && <p>SHELTER INFO</p>}
+            {!token && <p className="result__p">{userName}</p>}
+            {!token && <p className="result__p">{email}</p>}
+            {!token && <p className="result__p">{phone}</p>}
+            {!token && <p className="result__p">{descriptionShelter}</p>}
 
             {token && <button className="result__button" onClick={()=>handleDeletePet(id) }>DELETE PET</button>}
             </div>
