62e9aa66e9313660a95983a009433397a4d390b9
Siscu Cano <siscuca@gmail.com>
2020-12-14 22:31:34 +0100

    Test QR Ref

--- staff/siscu-cano/geogin-project/geogin-app/src/pages/FavoritesList.jsx
+++ staff/siscu-cano/geogin-project/geogin-app/src/pages/FavoritesList.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import { useBodyClass } from '../hooks/useBodyClass'
 
 const QRCode = require('qrcode.react')
 
 export const FavoritesList = () => {
+  const qrRef = createRef()
+
   useBodyClass('favoriteslist')
+
+  const handleQrCode = () => {
+    console.log(qrRef)
+    console.log(qrRef.current)
+    console.log(qrRef.innerHTML)
+  }
+
   return (
     <>
       <h1>Componente: FavoritesList</h1>
-      <QRCode renderAs='svg' value='http://facebook.github.io/react/' />
+      <div ref={qrRef}>
+        <QRCode renderAs='svg' value='http://facebook.github.io/react/' />
+      </div>
 
+      <button onClick={handleQrCode}>Focus the text input</button>
     </>
   )
 }
