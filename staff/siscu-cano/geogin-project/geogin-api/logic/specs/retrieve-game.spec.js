35676e67906070a7e293db58450998d852494a7c
siscu <siscuca@gmail.com>
2020-12-23 08:56:58 +0100

    Add retrieve game specs

--- staff/siscu-cano/geogin-project/geogin-api/logic/specs/retrieve-game.spec.js
+++ staff/siscu-cano/geogin-project/geogin-api/logic/specs/retrieve-game.spec.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const chai = require('chai')
 const { expect } = chai
 const chaiSubset = require('chai-subset');
+const { ContentError, LengthError } = require('geogin-errors')
 
 const {
   randomStringWithPrefix,
@@ -142,6 +143,48 @@ describe('retrieveGame()', () => {
 
 
   }) // End describe
+
+  describe('when game id is wrong', () => {
+    describe('when game id is not a string', () => {
+        let gameId
+
+        beforeEach(() => gameId = [true, 123, null, undefined, {}, function () { }, []].random())
+
+        it('should fail on non-string game id', () => {
+            expect(() => retrieveGame(gameId, () => { })).to.throw(TypeError, `${gameId} is not an id`)
+        })
+    })
+
+    describe('when game id is empty or blank', () => {
+        let gameId
+
+        beforeEach(() => gameId = ['', ' ', '\t', '\t', '\r'].random())
+
+        it('should fail on empty or blank game id', () => {
+            expect(() => retrieveGame(gameId, () => { })).to.throw(ContentError, `id is empty or blank`)
+        })
+    })
+
+    describe('when game id length is not 24', () => {
+        let gameId
+
+        beforeEach(() => gameId = ['a', 'b', 'c'].random().repeat(24 + (Math.random() > 0.5 ? 3 : 3)))
+
+        it('should fail on game id length different from 24', () => {
+            expect(() => retrieveGame(gameId, () => { })).to.throw(LengthError, `${gameId} length ${gameId.length} is not 24`)
+        })
+    })
+
+    describe('when game id is not hexadecimal', () => {
+      let gameId
+
+      beforeEach(() => gameId = '00000000000000000000000J')
+
+      it('should fail on game id length different from 24', () => {
+          expect(() => retrieveGame(gameId, () => { })).to.throw(TypeError, `is not a valid id`)
+      })
+  })
+})
 
   after(mongoose.disconnect)
 })
