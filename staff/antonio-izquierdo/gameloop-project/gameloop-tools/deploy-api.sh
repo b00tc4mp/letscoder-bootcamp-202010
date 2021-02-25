585a5fe20b1e3df7293a5feb2c0047700b5fe6f2
Toño <salvatuidea@gmail.com>
2021-02-25 12:16:58 +0100

    .

--- staff/antonio-izquierdo/gameloop-project/gameloop-tools/deploy-api.sh
+++ staff/antonio-izquierdo/gameloop-project/gameloop-tools/deploy-api.sh
@@ -1,14 +1,18 @@
-rm-rf ~/Desktop/gameloop-api-deploy
+#rm-rf ~/Desktop/gameloop-api-deploy
 
-mkdir ~/Desktop/gameloop-api-deploy
+#mkdir ~/Desktop/gameloop-api-deploy
 
-git init ~/Desktop/gameloop-api-deploy
+#git init ~/Desktop/gameloop-api-deploy
 
-cp -r ./gameloop-api ~/Desktop/gameloop-api-deploy
-cp -r ./gameloop-data ~/Desktop/gameloop-api-deploy
-cp -r ./gameloop-doc ~/Desktop/gameloop-api-deploy
-cp -r ./gameloop-errors ~/Desktop/gameloop-api-deploy
+#cp -r ../gameloop-api ~/Desktop/gameloop-api-deploy
+#cp -r ../gameloop-data ~/Desktop/gameloop-api-deploy
+#cp -r ../gameloop-doc ~/Desktop/gameloop-api-deploy
+#cp -r ../gameloop-errors ~/Desktop/gameloop-api-deploy
 
-cd ~/Desktop/gameloop-api-deploy
+#cd ~/Desktop/gameloop-api-deploy
 
-heroku create gameloop-api-server
+#heroku create gameloop-api-server
+
+cp ./gameloop-tools/package.json ~/Desktop/gameloop-api-deploy
+
+git add --all; git commit -m 'initial deploy'; git push heroku master
