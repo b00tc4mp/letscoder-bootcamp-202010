96c5d79195598ee870b937314c0c4fab70be4568
Toño <salvatuidea@gmail.com>
2021-02-25 12:16:43 +0100

    .

--- staff/antonio-izquierdo/gameloop-project/gameloop-tools/deploy-api.sh
+++ staff/antonio-izquierdo/gameloop-project/gameloop-tools/deploy-api.sh
@@ -6,6 +6,7 @@ git init ~/Desktop/gameloop-api-deploy
 
 cp -r ./gameloop-api ~/Desktop/gameloop-api-deploy
 cp -r ./gameloop-data ~/Desktop/gameloop-api-deploy
+cp -r ./gameloop-doc ~/Desktop/gameloop-api-deploy
 cp -r ./gameloop-errors ~/Desktop/gameloop-api-deploy
 
 cd ~/Desktop/gameloop-api-deploy
